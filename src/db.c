723732bbadf11867842939b643801598cf98bc5c
jnc <jnc@noemail.net>
2007-09-23 22:27:55 +0000

    Fixed bug in open_cmd. As of ff4c, open now runs update --latest as well, which would err on a brand new, blank, database. open_cmd now only runs update --latest if the current pid != 0
    FossilOrigin-Name: 4dc622f36c5aec900cd8039d4087f578afe31eb3

--- src/db.c
+++ src/db.c
@@ -890,9 +890,11 @@ void cmd_open(void){
   vid = db_int(0, "SELECT pid FROM plink y"
                   " WHERE NOT EXISTS(SELECT 1 FROM plink x WHERE x.cid=y.pid)");
   db_lset_int("checkout", vid);
-  g.argv = azNewArgv;
-  g.argc = 3;
-  update_cmd();
+  if( vid!=0 ){
+    g.argv = azNewArgv;
+    g.argc = 3;
+    update_cmd();
+  }
 }
 
 /*
