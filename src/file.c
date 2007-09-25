232ffc7c0d8a358134b77e85989ee9bc4594ad6a
jnc <jnc@noemail.net>
2007-09-25 00:17:39 +0000

    When checking for win32 absolute paths, I added a check to make sure we don't overstep the bounds of a string
    FossilOrigin-Name: af3a751841e268336fea30c2d6499c2d85e6d71e

--- src/file.c
+++ src/file.c
@@ -184,7 +184,8 @@ int file_simplify_name(char *z, int n){
 ** Convert /A/../ to just /
 */
 void file_canonical_name(const char *zOrigName, Blob *pOut){
-  if( zOrigName[0]=='/' || (zOrigName[1]==':' && zOrigName[2]=='\\') ){
+  if( zOrigName[0]=='/' 
+      || (strlen(zOrigName)>3 && zOrigName[1]==':' && zOrigName[2]=='\\') ){
     blob_set(pOut, zOrigName);
     blob_materialize(pOut);
   }else{
