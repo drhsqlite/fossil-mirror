ffa4f25a2eace3e95d491bc4368b0bc0890ff9f5
aku <aku@noemail.net>
2007-10-10 03:28:21 +0000

    Added a lot of skeleton files for the revision and symbol data structures, for both project and file level.
    FossilOrigin-Name: 84de38d73fe60325c221937c8b1612c0da9e7171

---
+++ tools/cvs2fossil/lib/c2f_frev.tcl
@@ -0,0 +1,57 @@
+## -*- tcl -*-
+# # ## ### ##### ######## ############# #####################
+## Copyright (c) 2007 Andreas Kupries.
+#
+# This software is licensed as described in the file LICENSE, which
+# you should have received as part of this distribution.
+#
+# This software consists of voluntary contributions made by many
+# individuals.  For exact contribution history, see the revision
+# history and logs, available at http://fossil-scm.hwaci.com/fossil
+# # ## ### ##### ######## ############# #####################
+
+## Revisions per file.
+
+# # ## ### ##### ######## ############# #####################
+## Requirements
+
+package require Tcl 8.4                             ; # Required runtime.
+package require snit                                ; # OO system.
+
+# # ## ### ##### ######## ############# #####################
+## 
+
+snit::type ::vc::fossil::import::cvs::file::rev {
+    # # ## ### ##### ######## #############
+    ## Public API
+
+    constructor {} {
+	return
+    }
+
+    # # ## ### ##### ######## #############
+    ## State
+
+    # # ## ### ##### ######## #############
+    ## Internal methods
+
+    # # ## ### ##### ######## #############
+    ## Configuration
+
+    pragma -hastypeinfo    no  ; # no type introspection
+    pragma -hasinfo        no  ; # no object introspection
+    pragma -hastypemethods no  ; # type is not relevant.
+    pragma -simpledispatch yes ; # simple fast dispatch
+
+    # # ## ### ##### ######## #############
+}
+
+namespace eval ::vc::fossil::import::cvs::file {
+    namespace export rev
+}
+
+# # ## ### ##### ######## ############# #####################
+## Ready
+
+package provide vc::fossil::import::cvs::file::rev 1.0
+return
