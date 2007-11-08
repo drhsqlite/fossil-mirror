1d2f5c78881761c978aa7b9ff8b2383b55a38f99
drh <drh@noemail.net>
2007-11-08 16:14:12 +0000

    Add the --detail flag to the merge command.
    FossilOrigin-Name: 50ff86afd0ea05c68d5cb7ec67e7d64df1bc92ac

--- src/merge.c
+++ src/merge.c
@@ -46,7 +46,9 @@ void merge_cmd(void){
   int mid;              /* Version we are merging against */
   int pid;              /* The pivot version - most recent common ancestor */
   Stmt q;
+  int detailFlag;
 
+  detailFlag = find_option("detail",0,0)!=0;
   if( g.argc!=3 ){
     usage("VERSION");
   }
@@ -217,7 +219,7 @@ void merge_cmd(void){
   ** Do a three-way merge on files that have changes pid->mid and pid->vid
   */
   db_prepare(&q,
-    "SELECT ridm, idv, ridp FROM fv"
+    "SELECT ridm, idv, ridp, ridv FROM fv"
     " WHERE idp>0 AND idv>0 AND idm>0"
     "   AND ridm!=ridp AND (ridv!=ridp OR chnged)"
   );
@@ -225,11 +227,16 @@ void merge_cmd(void){
     int ridm = db_column_int(&q, 0);
     int idv = db_column_int(&q, 1);
     int ridp = db_column_int(&q, 2);
+    int ridv = db_column_int(&q, 3);
     char *zName = db_text(0, "SELECT pathname FROM vfile WHERE id=%d", idv);
     char *zFullPath;
     Blob m, p, v, r;
     /* Do a 3-way merge of idp->idm into idp->idv.  The results go into idv. */
-    printf("MERGE %s\n", zName);
+    if( detailFlag ){
+      printf("MERGE %s  (pivot=%d v1=%d v2=%d)\n", zName, ridp, ridm, ridv);
+    }else{
+      printf("MERGE %s\n", zName);
+    }
     undo_save(zName);
     zFullPath = mprintf("%s/%s", g.zLocalRoot, zName);
     free(zName);
