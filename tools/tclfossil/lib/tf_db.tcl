a4eff2402ebf22f233a54b3176029ef722f71c87
mjanssen <mjanssen@noemail.net>
2008-02-03 18:18:42 +0000

    tclfossil: added revlist command
    FossilOrigin-Name: 1c92462c04fa3fdd1932de154966fba9c481dc2c

--- tools/tclfossil/lib/tf_db.tcl
+++ tools/tclfossil/lib/tf_db.tcl
@@ -3,6 +3,17 @@ package require sqlite3
 package require snit
 
 snit::type ::vc::fossil::db {
-    typemethod open_repository {{name {}}} {
+    variable db
+    method open_repository {{name {}}} {
+	sqlite3 db1 c:/src/fossil.fsl
+        set db db1
+    }
+    method revlist {} {
+	$db eval {select uuid from blob}
     }
 }
+
+vc::fossil::db create fossildb
+
+fossildb open_repository
+puts [fossildb revlist]
