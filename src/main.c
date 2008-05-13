27053240de108508acf278fefc4625b4f9317b8d
drh <drh@noemail.net>
2008-05-13 00:19:15 +0000

    Use the SCRIPT_NAME environment variable rather than REQUEST_URI to find the base name of the CGI script.
    FossilOrigin-Name: c23469468bc6a8a47bb33bb94e61dcaa5058d3d9

--- src/main.c
+++ src/main.c
@@ -429,21 +429,10 @@ void set_base_url(void){
   int i;
   const char *zHost = PD("HTTP_HOST","");
   const char *zMode = PD("HTTPS","off");
-  const char *zCur = PD("REQUEST_URI","/");
+  const char *zCur = PD("SCRIPT_NAME","/");
 
-  for(i=0; zCur[i] && zCur[i]!='?' && zCur[i]!='#'; i++){}
-  if( g.zExtra ){
-    /* Skip to start of extra stuff, then pass over any /'s that might
-    ** have separated the document root from the extra stuff. This
-    ** ensures that the redirection actually redirects the root, not
-    ** something deep down at the bottom of a URL.
-    */
-    i -= strlen(g.zExtra);
-    while( i>0 && zCur[i-1]=='/' ){ i--; }
-  }
-  while( i>0 && zCur[i-1]!='/' ){ i--; }
-  while( i>0 && zCur[i-1]=='/' ){ i--; }
-
+  i = strlen(zCur);
+  while( i>0 && zCur[i-1]=='/' ) i--;
   if( strcmp(zMode,"on")==0 ){
     g.zBaseURL = mprintf("https://%s%.*s", zHost, i, zCur);
     g.zTop = &g.zBaseURL[8+strlen(zHost)];
