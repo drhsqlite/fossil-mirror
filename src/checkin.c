13c2c0cd715f653b105ff81947cb37340b6d116f
drh <drh@noemail.net>
2008-06-06 20:05:06 +0000

    Fix to the omitsign setting.
    FossilOrigin-Name: 417e7662e5232e58af3cabcc81f6294a24239d66

--- src/checkin.c
+++ src/checkin.c
@@ -349,7 +349,7 @@ void commit_cmd(void){
   zComment = find_option("comment","m",1);
   forceFlag = find_option("force", "f", 0)!=0;
   db_must_be_within_tree();
-  noSign = db_get_int("omitsign", 0)|noSign;
+  noSign = db_get_boolean("omitsign", 0)|noSign;
   verify_all_options();
 
   /*
