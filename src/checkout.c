0ed901730b6c442924b1b381a6fc6ed5a3d231c8
drh <drh@noemail.net>
2009-10-31 13:11:34 +0000

    Make use of the is_a_version() function to check for valid version IDs in the checkout command.  Ticket [2de4ae605844fe3f].
    FossilOrigin-Name: 999b1779e71ed659e7b962ae96905bd9a6a5dcc6

--- src/checkout.c
+++ src/checkout.c
@@ -75,7 +75,7 @@ int load_vfile(const char *zName){
   if( vid==0 ){
     fossil_fatal("no such check-in: %s", g.argv[2]);
   }
-  if( !db_exists("SELECT 1 FROM mlink WHERE mid=%d", vid) ){
+  if( !is_a_version(vid) ){
     fossil_fatal("object [%.10s] is not a check-in", blob_str(&uuid));
   }
   load_vfile_from_rid(vid);
