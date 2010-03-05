3a4d4bcb420723b36183be380cc389b8c58f2f66
drh <drh@noemail.net>
2010-03-05 21:20:30 +0000

    Another attempt to get the quoting right for RSS feeds.
    FossilOrigin-Name: 18449fa6e6acafeeabc40fe8b7c225358b524fd4

--- src/rss.c
+++ src/rss.c
@@ -133,9 +133,9 @@ void page_timeline_rss(void){
     }
 
     @     <item>
-    @       <title>%s(zPrefix)%w(zCom)</title>
+    @       <title>%s(zPrefix)%h(zCom)</title>
     @       <link>%s(g.zBaseURL)/ci/%s(zId)</link>
-    @       <description>%s(zPrefix)%w(zCom)</description>
+    @       <description>%s(zPrefix)%h(zCom)</description>
     @       <pubDate>%s(zDate)</pubDate>
     @       <author>%h(zAuthor)</author>
     @       <guid>%s(g.zBaseURL)/ci/%s(zId)</guid>
