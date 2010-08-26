b4eaa6deed5204f72281190dba82e053905040bd
drh <drh@noemail.net>
2010-08-26 13:17:17 +0000

    Untested implementation of popen2() for windows.
    FossilOrigin-Name: 34ea1e4abb5e86d8ccaf81ac38b77c6c56197717

--- src/popen.c
+++ src/popen.c
@@ -20,6 +20,67 @@
 #include "config.h"
 #include "popen.h"
 
+#ifdef __MINGW32__
+/*
+** Print a fatal error and quit.
+*/
+static void win32_fatal_error(const char *zMsg){
+  fossil_fatal("%s");
+}
+#endif
+
+
+
+#ifdef __MINGW32__
+/*
+** On windows, create a child process and specify the stdin, stdout,
+** and stderr channels for that process to use.
+**
+** Return the number of errors.
+*/
+static int win32_create_child_process(
+  char *zCmd,          /* The command that the child process will run */
+  HANDLE hIn,          /* Standard input */
+  HANDLE hOut,         /* Standard output */
+  HANDLE hErr,         /* Standard error */
+  DWORD *pChildPid     /* OUT: Child process handle */
+){
+  STARTUPINFO si;
+  PROCESS_INFORMATION pi;
+  BOOL rc;
+
+  memset(&si, 0, sizeof(si));
+  si.cb = sizeof(si);
+  si.dwFlags = STARTF_USESTDHANDLES;
+  SetHandleInformation(hIn, HANDLE_FLAG_INHERIT, TRUE);
+  si.hStdInput  = hIn;
+  SetHandleInformation(hOut, HANDLE_FLAG_INHERIT, TRUE);
+  si.hStdOutput = hOut;
+  SetHandleInformation(hErr, HANDLE_FLAG_INHERIT, TRUE);
+  si.hStdError  = hErr;
+  rc = CreateProcess(
+     NULL,  /* Application Name */
+     zCmd,  /* Command-line */
+     NULL,  /* Process attributes */
+     NULL,  /* Thread attributes */
+     TRUE,  /* Inherit Handles */
+     0,     /* Create flags  */
+     NULL,  /* Environment */
+     NULL,  /* Current directory */
+     &si,   /* Startup Info */
+     &pi    /* Process Info */
+  );
+  if( rc ){
+    CloseHandle( pi.hProcess );
+    CloseHandle( pi.hThread );
+    *pChildPid = pi.dwProcessId;
+  }else{
+    win32_fatal_error("cannot create child process");
+  }
+  return rc!=0;
+}
+#endif
+
 /*
 ** Create a child process running shell command "zCmd".  *ppOut is
 ** a FILE that becomes the standard input of the child process.  
@@ -33,7 +94,34 @@
 */
 int popen2(const char *zCmd, int *pfdIn, FILE **ppOut, int *pChildPid){
 #ifdef __MINGW32__
-  return 1;   /* Not implemented on windows, yet */
+  HANDLE hStdinRd, hStdinWr, hStdoutRd, hStdoutWr, hStderr;
+  SECURITY_ATTRIBUTES saAttr;    
+  DWORD childPid;
+  int fd;
+
+  saAttr.nLength = sizeof(saAttr);
+  saAttr.bInheritHandle = TRUE;
+  saAttr.lpSecurityDescriptor = NULL; 
+  hStderr = GetStdHandle(STD_ERROR_HANDLE);
+  if( !CreatePipe(&hStdoutRd, &hStdoutWr, &saAttr, 4096) ){
+    win32_fatal_error("cannot create pipe for stdout");
+  }
+  SetHandleInformation( hStdoutRd, HANDLE_FLAG_INHERIT, FALSE);
+
+  if( !CreatePipe(&hStdinRd, &hStdinWr, &saAttr, 4096) ){
+    win32_fatal_error("cannot create pipe for stdin");
+  }
+  SetHandleInformation( hStdinWr, HANDLE_FLAG_INHERIT, FALSE);
+  
+  win32_create_child_process((char*)zCmd, 
+                             hStdinRd, hStdoutWr, hStderr,&childPid);
+  *pChildPid = childPid;
+  *pfdIn = _open_osfhandle(hStdoutRd, 0);
+  fd = _open_osfhandle(hStdoutWr, 0);
+  *ppOut = _fdopen(fd, "w");
+  CloseHandle(hStdinRd); 
+  CloseHandle(hStdoutWr);
+  return 0;
 #else
   int pin[2], pout[2];
   *pfdIn = 0;
