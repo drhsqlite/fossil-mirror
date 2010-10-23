734bf8051e523619a9f2b2daa8438ae62fa5ddf0
drh <drh@noemail.net>
2010-10-23 02:51:10 +0000

    Simplification to the delta checksum algorithm.  Trying to make it run faster.
    FossilOrigin-Name: 4c1bcf6f6aa7b906841251bbc3c18d84626849f3

--- src/delta.c
+++ src/delta.c
@@ -197,20 +197,20 @@ static int digit_count(int v){
 */
 static unsigned int checksum(const char *zIn, size_t N){
   const unsigned char *z = (const unsigned char *)zIn;
-  unsigned sum = 0;
-  while(N >= 16){
-    sum += ((unsigned)z[0] + z[4] + z[8] + z[12]) << 24;
-    sum += ((unsigned)z[1] + z[5] + z[9] + z[13]) << 16;
-    sum += ((unsigned)z[2] + z[6] + z[10]+ z[14]) << 8;
-    sum += ((unsigned)z[3] + z[7] + z[11]+ z[15]);
-    z += 16;
-    N -= 16;
-  }
-  while(N >= 4){
-    sum += (z[0]<<24) | (z[1]<<16) | (z[2]<<8) | z[3];
-    z += 4;
+  unsigned sum0 = 0;
+  unsigned sum1 = 0;
+  unsigned sum2 = 0;
+  unsigned sum3 = 0;
+  unsigned sum;
+  while( N>=4 ){
+    sum0 += z[0];
+    sum1 += z[1];
+    sum2 += z[2];
+    sum3 += z[3];
     N -= 4;
+    z += 4;
   }
+  sum = (sum0 << 24) + (sum1 << 16) + (sum2 << 8) + sum3;
   switch(N){
     case 3:   sum += (z[2] << 8);
     case 2:   sum += (z[1] << 16);
