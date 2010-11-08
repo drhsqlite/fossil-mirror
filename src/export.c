b57244f48e005d1aacd2790624cc175d0022ba96
drh <drh@noemail.net>
2010-11-08 23:26:47 +0000

    Export works for Fossil and SQLite repositories.  Does not yet export tags. Also need to add incremental export.
    FossilOrigin-Name: e25884d44f59221a6ab89404db8206d6863b16a2

--- src/export.c
+++ src/export.c
@@ -24,7 +24,7 @@
 /*
 ** Output a "committer" record for the given user.
 */
-static void print_committer(const char *zUser){
+static void print_person(const char *zUser){
   static Stmt q;
   const char *zContact;
   char *zName;
@@ -32,7 +32,7 @@ static void print_committer(const char *zUser){
   int i, j;
 
   if( zUser==0 ){
-    printf("committer unknown <unknown>");
+    printf(" <unknown>");
     return;
   }
   db_static_prepare(&q, "SELECT info FROM user WHERE login=:user");
@@ -41,7 +41,7 @@ static void print_committer(const char *zUser){
     db_reset(&q);
     for(i=0; zUser[i] && zUser[i]!='>' && zUser[i]!='<'; i++){}
     if( zUser[i]==0 ){
-      printf("committer %s <unknown>", zUser);
+      printf(" <%s>", zUser);
       return;
     }
     zName = mprintf("%s", zUser);
@@ -51,14 +51,14 @@ static void print_committer(const char *zUser){
       }
     }
     zName[j] = 0;
-    printf("committer %s <unknown>", zName);
+    printf(" %s <%s>", zName, zUser);
     free(zName);
     return;
   }
   zContact = db_column_text(&q, 0);
   for(i=0; zContact[i] && zContact[i]!='>' && zContact[i]!='<'; i++){}
   if( zContact[i]==0 ){
-    printf("committer %s <unknown>", zContact);
+    printf(" %s <%s>", zContact, zUser);
     db_reset(&q);
     return;
   }
@@ -67,14 +67,14 @@ static void print_committer(const char *zUser){
     for(i=0; zEmail[i] && zEmail[i]!='>'; i++){}
     if( zEmail[i]=='>' ) zEmail[i+1] = 0;
   }else{
-    zEmail = mprintf("<unknown");
+    zEmail = mprintf("<%s>", zUser);
   }
   zName = mprintf("%.*s", i, zContact);
   for(i=j=0; zName[i]; i++){
     if( zName[i]!='"' ) zName[j++] = zName[i];
   }
   zName[j] = 0;
-  printf("committer %s %s", zName, zEmail);
+  printf(" %s %s", zName, zEmail);
   free(zName);
   free(zEmail);
   db_reset(&q);
@@ -98,18 +98,22 @@ static void print_committer(const char *zUser){
 void export_cmd(void){
   Stmt q;
   int i;
+  Bag blobs, vers;
+  bag_init(&blobs);
+  bag_init(&vers);
 
   db_find_and_open_repository(1);
 
   /* Step 1:  Generate "blob" records for every artifact that is part
   ** of a check-in 
   */
-  db_prepare(&q, "SELECT DISTINCT fid FROM mlink");
+  db_prepare(&q, "SELECT DISTINCT fid FROM mlink WHERE fid>0");
   while( db_step(&q)==SQLITE_ROW ){
     int rid = db_column_int(&q, 0);
     Blob content;
     content_get(rid, &content);
     printf("blob\nmark :%d\ndata %d\n", rid, blob_size(&content));
+    bag_insert(&blobs, rid);
     fwrite(blob_buffer(&content), 1, blob_size(&content), stdout);
     printf("\n");
     blob_reset(&content);
@@ -133,28 +137,46 @@ void export_cmd(void){
     const char *zComment = db_column_text(&q, 2);
     const char *zUser = db_column_text(&q, 3);
     const char *zBranch = db_column_text(&q, 4);
+    char *zBr;
     Manifest *p;
     ManifestFile *pFile;
+    const char *zFromType;
 
+    bag_insert(&vers, ckinId);
     if( zBranch==0 ) zBranch = "trunk";
-    printf("commit refs/heads/%s\nmark :%d\n", zBranch, ckinId);
-    print_committer(zUser); printf(" %lld +0000\n", secondsSince1970);
+    zBr = mprintf("%s", zBranch);
+    for(i=0; zBr[i]; i++){
+      if( !fossil_isalnum(zBr[i]) ) zBr[i] = '_';
+    }
+    printf("commit refs/heads/%s\nmark :%d\n", zBr, ckinId);
+    free(zBr);
+    printf("committer");
+    print_person(zUser);
+    printf(" %lld +0000\n", secondsSince1970);
     if( zComment==0 ) zComment = "null comment";
     printf("data %d\n%s\n", strlen(zComment), zComment);
     p = manifest_get(ckinId, CFTYPE_ANY);
+    zFromType = "from";
     for(i=0; i<p->nParent; i++){
-      printf("%s :%d\n", (i?"merge":"from"), fast_uuid_to_rid(p->azParent[i]));
+      int pid = fast_uuid_to_rid(p->azParent[i]);
+      if( pid==0 || !bag_find(&vers, pid) ) continue;
+      printf("%s :%d\n", zFromType, fast_uuid_to_rid(p->azParent[i]));
+      zFromType = "merge";
     }
     printf("deleteall\n");
     manifest_file_rewind(p);
     while( (pFile=manifest_file_next(p, 0))!=0 ){
+      int fid = fast_uuid_to_rid(pFile->zUuid);
       const char *zPerm = "100644";
+      if( fid==0 ) continue;
       if( pFile->zPerm && strstr(pFile->zPerm,"x") ) zPerm = "100755";
-      printf("M %s :%d %s\n",
-             zPerm, fast_uuid_to_rid(pFile->zUuid), pFile->zName);
+      if( !bag_find(&blobs, fid) ) continue;
+      printf("M %s :%d %s\n", zPerm, fid, pFile->zName);
     }
     manifest_destroy(p);
     printf("\n");
   }
   db_finalize(&q);
+  bag_clear(&blobs);
+  bag_clear(&vers);
 }
