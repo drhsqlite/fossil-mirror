729bf57dcfcdab87907927bff937fdd16d0b5999
drh <drh@noemail.net>
2011-02-07 20:59:31 +0000

    Transfer the content of the ignore-glob setting as part of the "proj" configuration.  Ticket [1b5773eed1758a03a].
    FossilOrigin-Name: 7bb3db9b64b8c404c210a102ca8165dba300e906

--- src/configure.c
+++ src/configure.c
@@ -77,6 +77,7 @@ static struct {
   { "project-name",           CONFIGSET_PROJ },
   { "project-description",    CONFIGSET_PROJ },
   { "manifest",               CONFIGSET_PROJ },
+  { "ignore-glob",            CONFIGSET_PROJ },
   { "index-page",             CONFIGSET_SKIN },
   { "timeline-block-markup",  CONFIGSET_SKIN },
   { "timeline-max-comment",   CONFIGSET_SKIN },
