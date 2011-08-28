7c7b3f7a781d181c17f82aba48e3ec70aafae4ff
dmitry <dmitry@noemail.net>
2011-08-28 10:43:26 +0000

    Handle errors in blob_read_link().
    FossilOrigin-Name: d43029f6f450fbbea36199e5abdec18972c628fd

--- src/blob.c
+++ src/blob.c
@@ -713,17 +713,17 @@ int blob_read_from_file(Blob *pBlob, const char *zFilename){
 ** On windows, zeros blob and returns 0.
 */
 int blob_read_link(Blob *pBlob, const char *zFilename){
+  blob_zero(pBlob);
 #if !defined(_WIN32)
   char zBuf[1024];
   ssize_t len = readlink(zFilename, zBuf, 1023);
-  blob_zero(pBlob);
-  if (len > 0) {
-    zBuf[len] = 0;
-    blob_appendf(pBlob, "%s", zBuf);
+  if( len < 0 ){
+    fossil_panic("cannot read symbolic link %s", zFilename);
   }
+  zBuf[len] = 0;   /* null-terminate */
+  blob_appendf(pBlob, "%s", zBuf);
   return len;
 #else
-  blob_zero(pBlob);
   return 0;
 #endif
 }
