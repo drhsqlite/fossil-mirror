ba81521ee7ef04302df630bb23c04f857feac112
dmitry <dmitry@noemail.net>
2011-09-02 10:34:59 +0000

    Print description of conflict when 'stash apply' cannot merge a symlink and a regular file.
    FossilOrigin-Name: bca6924e5ff67a5ffa89517a81a357d0b4cef024

--- src/stash.c
+++ src/stash.c
@@ -234,13 +234,13 @@ static void stash_apply(int stashid, int nConflict){
         if( isLink || isNewLink ){
           rc = -1;
           blob_zero(&b); /* because we reset it later */
-          //TODO(dchest): write something to disk?
+          fossil_print("***** Cannot merge symlink %s\n", zNew);
         }else{
           rc = merge_3way(&a, zOPath, &b, &out);
           blob_write_to_file(&out, zNPath);          
-          //blob_reset(&out); //XXX(dchest): Need this?
+          blob_reset(&out);
+          file_setexe(zNPath, isExec);
         }
-        file_setexe(zNPath, isExec);
         if( rc ){
           fossil_print("CONFLICT %s\n", zNew);
           nConflict++;
