189a0b30365963117b125f1f66c8addb8a11bec5
drh <drh@noemail.net>
2011-09-14 13:45:53 +0000

    Update the built-in SQLite amalgamation to the latest 3.7.8 beta.
    FossilOrigin-Name: a0cf985a42a7552525c74a8e5d6c6427f9cc0146

--- src/sqlite3.h
+++ src/sqlite3.h
@@ -109,7 +109,7 @@ extern "C" {
 */
 #define SQLITE_VERSION        "3.7.8"
 #define SQLITE_VERSION_NUMBER 3007008
-#define SQLITE_SOURCE_ID      "2011-09-04 01:27:00 6b657ae75035eb10b0ad640998d3c9eadfdffa6e"
+#define SQLITE_SOURCE_ID      "2011-09-14 13:41:40 a1f3aeeb0988c848d40ce8f6da6e902935a997e2"
 
 /*
 ** CAPI3REF: Run-Time Library Version Numbers
