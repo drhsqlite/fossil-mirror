11624ec0417f5517ab3880ad72b5fb9eb004b90f
stephan <stephan@noemail.net>
2011-10-02 10:51:22 +0000

    Added missing license header to new files. Updated (c) date to 2011.
    FossilOrigin-Name: 8a665715c247434fc20474cd556987210e36dbd9

--- src/json_detail.h
+++ src/json_detail.h
@@ -1,5 +1,21 @@
 #if !defined(FOSSIL_JSON_DETAIL_H_INCLUDED)
 #define FOSSIL_JSON_DETAIL_H_INCLUDED
+/*
+** Copyright (c) 2011 D. Richard Hipp
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the Simplified BSD License (also
+** known as the "2-Clause License" or "FreeBSD License".)
+**
+** This program is distributed in the hope that it will be useful,
+** but without any warranty; without even the implied warranty of
+** merchantability or fitness for a particular purpose.
+**
+** Author contact information:
+**   drh@hwaci.com
+**   http://www.hwaci.com/drh/
+**
+*/
 
 #include "cson_amalgamation.h"
 /*
