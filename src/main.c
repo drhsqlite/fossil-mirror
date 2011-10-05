84b1da7c68f5f1a834a9ef179ca2b9d3389202fb
stephan <stephan@noemail.net>
2011-10-05 08:00:00 +0000

    Added (--args -) to read from stdin. Theoretically now also strips windows newlines, but i do not have a broken editor to test that with ;).
    FossilOrigin-Name: 080d27a6b2bcf684bd266b313b4618bff9298a14

--- src/main.c
+++ src/main.c
@@ -251,7 +251,8 @@ static void expand_args_option(void){
   int n;                    /* Number of bytes in one line */
   char *z;            /* General use string pointer */
   char **newArgv;     /* New expanded g.argv under construction */
-
+  char const * zFileName;   /* input file name */
+  FILE * zInFile;           /* input FILE */
   for(i=1; i<g.argc-1; i++){
     z = g.argv[i];
     if( z[0]!='-' ) continue;
@@ -262,7 +263,19 @@ static void expand_args_option(void){
   }
   if( i>=g.argc-1 ) return;
 
-  blob_read_from_file(&file, g.argv[i+1]);
+  zFileName = g.argv[i+1];
+  zInFile = (0==strcmp("-",zFileName))
+    ? stdin
+    : fopen(zFileName,"rb");
+  if(!zInFile){
+    fossil_panic("Cannot open -args file [%s]", zFileName);
+  }else{
+    blob_read_from_channel(&file, zInFile, -1);
+    if(stdin != zInFile){
+      fclose(zInFile);
+    }
+    zInFile = NULL;
+  }
   z = blob_str(&file);
   for(k=0, nLine=1; z[k]; k++) if( z[k]=='\n' ) nLine++;
   newArgv = fossil_malloc( sizeof(char*)*(g.argc + nLine*2) );
@@ -273,6 +286,10 @@ static void expand_args_option(void){
     if( n<=1 ) continue;
     z = blob_buffer(&line);
     z[n-1] = 0;
+    if((n>1) && ('\r'==z[n-2])){
+      if(n==2) continue /*empty line*/;
+      z[n-2] = 0;
+    }
     newArgv[j++] = z;
     if( z[0]=='-' ){
       for(k=1; z[k] && !fossil_isspace(z[k]); k++){}
