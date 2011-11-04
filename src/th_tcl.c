22b051c19d9d703f672c97e843bf67f240bbe58d
drh <drh@noemail.net>
2011-11-04 20:05:32 +0000

    Add the BSD copyright notice to the top of the th_tcl.c source file.
    FossilOrigin-Name: bf1ebbe1f647710aea7d6f4c9420219622763ab0

--- src/th_tcl.c
+++ src/th_tcl.c
@@ -1,4 +1,13 @@
 /*
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the Simplified BSD License (also
+** known as the "2-Clause License" or "FreeBSD License".)
+**
+** This program is distributed in the hope that it will be useful,
+** but without any warranty; without even the implied warranty of
+** merchantability or fitness for a particular purpose.
+**
+*******************************************************************************
 ** This file contains code used to bridge the TH1 and Tcl scripting languages.
 */
 
