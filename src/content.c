13531f7764afa929d54e7771ad7e3a07dcb3e4f3
drh <drh@noemail.net>
2012-02-10 13:13:41 +0000

    Report an error if the "artifact" command does not find an artifact.
    FossilOrigin-Name: 4e47bdc4a316c3d00e9ca07773a4a2e6cfed5547

--- src/content.c
+++ src/content.c
@@ -324,8 +324,11 @@ void artifact_cmd(void){
   if( g.argc!=4 && g.argc!=3 ) usage("ARTIFACT-ID ?FILENAME? ?OPTIONS?");
   zFile = g.argc==4 ? g.argv[3] : "-";
   rid = name_to_rid(g.argv[2]);
-  content_get(rid, &content);
-  blob_write_to_file(&content, zFile);
+  if( content_get(rid, &content) ){
+    blob_write_to_file(&content, zFile);
+  }else{
+    fossil_fatal("no such artifact: %s", g.argv[2]);
+  }
 }
 
 /*
