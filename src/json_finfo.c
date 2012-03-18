71362c9303b7bc3db34edb81076d40cfb2cd2b31
stephan <stephan@noemail.net>
2012-03-18 02:46:46 +0000

    Added uuid=... option to the as-yet-undocumented /json/finfo.
    FossilOrigin-Name: 64721991671aec654e5bf0e10e6ba18528d15b7c

--- src/json_finfo.c
+++ src/json_finfo.c
@@ -37,14 +37,20 @@ cson_value * json_page_finfo(){
   char const * zBefore = NULL;
   int limit = -1;
   int currentRow = 0;
+  char const * zCheckin = NULL;
   if(!g.perm.Read){
     json_set_err(FSL_JSON_E_DENIED,"Requires 'o' privileges.");
     return NULL;
   }
   json_warn( FSL_JSON_W_UNKNOWN, "Achtung: the output of the finfo command is up for change.");
-  zFilename = json_find_option_cstr2("name",NULL,NULL,
-                                 g.json.dispatchDepth+1);
 
+  /* For the "name" argument we have to jump through some hoops to make sure that we don't
+     get the fossil-internally-assigned "name" option.
+  */
+  zFilename = json_find_option_cstr("name",NULL,NULL);
+  if(!zFilename && !g.isHTTP){
+    zFilename = json_command_arg(g.json.dispatchDepth+1);
+  }
   if(!zFilename){
     json_set_err(FSL_JSON_E_MISSING_ARGS, "Missing 'name' parameter.");
     return NULL;
@@ -52,6 +58,7 @@ cson_value * json_page_finfo(){
   zBefore = json_find_option_cstr("before",NULL,"b");
   zAfter = json_find_option_cstr("after",NULL,"a");
   limit = json_find_option_int("limit",NULL,"n", -1);
+  zCheckin = json_find_option_cstr("checkin",NULL,"ci");
 
   blob_appendf(&sql, 
         "SELECT b.uuid,"
@@ -73,12 +80,28 @@ cson_value * json_page_finfo(){
         "   AND event.objid=ci.rid",
         zFilename, filename_collation()
                );
-  if( zAfter ){
-    blob_appendf(&sql, " AND event.mtime>=julianday('%q')", zAfter);
-  }else if( zBefore ){
-    blob_appendf(&sql, " AND event.mtime<=julianday('%q')", zBefore);
+
+  if( zCheckin && *zCheckin ){
+    char * zU = NULL;
+    int rc = name_to_uuid2( zCheckin, "ci", &zU );
+    printf("zCheckin=[%s], zU=[%s]", zCheckin, zU);
+    if(rc<=0){
+      json_set_err((rc<0) ? FSL_JSON_E_AMBIGUOUS_UUID : FSL_JSON_E_RESOURCE_NOT_FOUND,
+                   "Checkin UUID %s.", (rc<0) ? "is ambiguous" : "not found");
+      blob_reset(&sql);
+      return NULL;
+    }
+    blob_appendf(&sql, " AND ci.uuid='%q'", zU);
+    free(zU);
+  }else{
+    if( zAfter ){
+      blob_appendf(&sql, " AND event.mtime>=julianday('%q')", zAfter);
+    }else if( zBefore ){
+      blob_appendf(&sql, " AND event.mtime<=julianday('%q')", zBefore);
+    }
   }
   blob_appendf(&sql," ORDER BY event.mtime DESC /*sort*/");
+  /*printf("SQL=\n%s\n",blob_str(&sql));*/
   db_prepare(&q, "%s", blob_str(&sql)/*extra %s to avoid double-expanding
                                        SQL escapes*/);
   blob_reset(&sql);
@@ -94,7 +117,7 @@ cson_value * json_page_finfo(){
     cson_object * row = cson_new_object();
     cson_array_append( checkins, cson_object_value(row) );
     cson_object_set(row, "checkin", json_new_string( db_column_text(&q,1) ));
-    cson_object_set(row, "artifact", json_new_string( db_column_text(&q,2) ));
+    cson_object_set(row, "uuid", json_new_string( db_column_text(&q,2) ));
     /*cson_object_set(row, "parentArtifact", json_new_string( db_column_text(&q,6) ));*/
     cson_object_set(row, "mtime", json_new_int( db_column_int(&q,3) ));
     cson_object_set(row, "user", json_new_string( db_column_text(&q,4) ));
@@ -106,7 +129,16 @@ cson_value * json_page_finfo(){
   }
   db_finalize(&q);
 
-  return cson_object_value(pay);
+  if( !cson_array_length_get(checkins) ){
+    json_set_err(FSL_JSON_E_RESOURCE_NOT_FOUND,
+                 zCheckin
+                 ? "File not part of the given checkin."
+                 : "File not found." );
+    cson_free_object(pay);
+    pay = NULL;
+  }
+  
+  return pay ? cson_object_value(pay) : NULL;
 }
 
 
