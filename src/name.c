6446132c0acd35188d3f89060255a7c273e869cc
stephan <stephan@noemail.net>
2012-03-18 02:45:59 +0000

    Added name_to_uuid2()
    FossilOrigin-Name: c685a35eb08a1c4676df34018fc1f1f75804bf58

--- src/name.c
+++ src/name.c
@@ -269,6 +269,26 @@ int name_to_uuid(Blob *pName, int iErrPriority, const char *zType){
 }
 
 /*
+** This routine is similar to name_to_uuid() except in the form it
+** takes its parameters and returns its value, and in that it does not
+** treat errors as fatal. zName must be a UUID, as described for
+** name_to_uuid(). zType is also as described for that function. If
+** zName does not resolve, 0 is returned. If it is ambiguous, a
+** negative value is returned. On success the rid is returned and
+** pUuid (if it is not NULL) is set to the a newly-allocated string,
+** the full UUID, which must eventually be free()d by the caller.
+*/
+int name_to_uuid2(char const *zName, const char *zType, char **pUuid){
+  int rid = symbolic_name_to_rid(zName, zType);
+  if((rid>0) && pUuid){
+    *pUuid = db_text(NULL, "SELECT uuid FROM blob WHERE rid=%d", rid);
+  }
+  return rid;
+}
+
+
+
+/*
 ** COMMAND:  test-name-to-id
 **
 ** Convert a name to a full artifact ID.
