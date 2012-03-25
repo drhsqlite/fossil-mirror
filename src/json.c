49043826e4180430a704e11732ce15b06c89e4af
stephan <stephan@noemail.net>
2012-03-25 00:52:16 +0000

    switched from vmprintf() to sqlite3_mprintf() to get the extra percent format modifiers.
    FossilOrigin-Name: 3acdc5e448301cf942988ee7065477e87c369b3c

--- src/json.c
+++ src/json.c
@@ -380,10 +380,10 @@ cson_value * json_new_string_f( char const * fmt, ... ){
   char * zStr;
   va_list vargs;
   va_start(vargs,fmt);
-  zStr = vmprintf(fmt,vargs);
+  zStr = sqlite3_mprintf(fmt,vargs);
   va_end(vargs);
   v = cson_value_new_string(zStr, strlen(zStr));
-  free(zStr);
+  sqlite3_free(zStr);
   return v;  
 }
 
