ca932af49dfe41e1d047caa4124582e7de2d46c6
stephan <stephan@noemail.net>
2012-04-11 16:28:52 +0000

    Fixed: /tarball accepted non-checkin rids (but of course could not generate a tarfile from them). Bug report from Justin Gedge on the mailing list.
    FossilOrigin-Name: f982e3764b12866ca3d31893dc4a9c233b04684f

--- src/tar.c
+++ src/tar.c
@@ -594,7 +594,7 @@ void tarball_page(void){
       }
     }
   }
-  rid = name_to_rid(nRid?zRid:zName);
+  rid = name_to_typed_rid(nRid?zRid:zName, "ci");
   if( rid==0 ){
     @ Not found
     return;
