73e6dd154c7656725218b21ff08c95243d1c74e4
drh <drh@noemail.net>
2012-08-29 16:51:45 +0000

    An attempt to factor out the opendir() family of routines and provide portable alternatives, fossil_opendir().  This simplistic first attempt works on non-windows platforms, but the build fails on windows.
    FossilOrigin-Name: 8d8e529aa9a6295ee138cf510b94b4878aacb0e1

--- src/file.c
+++ src/file.c
@@ -1005,6 +1005,30 @@ int file_is_the_same(Blob *pContent, const char *zName){
   return rc==0;
 }
 
+/*
+** Portable unicode implementation of opendir()
+*/
+#if INTERFACE
+
+#if defined(_WIN32)
+# include <wdirent.h>
+# define FOSSIL_DIR WDIR
+# define fossil_dirent wdirent
+# define fossil_opendir wopendir
+# define fossil_readdir wreaddir
+# define fossil_closedir wclosedir
+#else
+# include <dirent.h>
+# define FOSSIL_DIR DIR
+# define fossil_dirent dirent
+# define fossil_opendir opendir
+# define fossil_readdir readdir
+# define fossil_closedir closedir
+#endif
+
+#endif /* INTERFACE */
+
+
 
 /**************************************************************************
 ** The following routines translate between MBCS and UTF8 on windows.
