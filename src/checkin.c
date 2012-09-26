5d02675a6b494f7385cd53a8f126be0080044a21
jan.nijtmans <jan.nijtmans@noemail.net>
2012-09-26 09:48:06 +0000

    Correct last merge: mbcs->utf-8 conversion is done later
    FossilOrigin-Name: b9640f05a249e594229f72e0833dca24c8696971

--- src/checkin.c
+++ src/checkin.c
@@ -517,13 +517,10 @@ static void prepare_commit_comment(
     char zIn[300];
     blob_reset(&text);
     while( fgets(zIn, sizeof(zIn), stdin)!=0 ){
-      char *zUtf8 = fossil_mbcs_to_utf8(zIn);
-      if( zUtf8[0]=='.' && (zUtf8[1]==0 || zUtf8[1]=='\r' || zUtf8[1]=='\n') ){
-        fossil_mbcs_free(zUtf8);
+      if( zIn[0]=='.' && (zIn[1]==0 || zIn[1]=='\r' || zIn[1]=='\n') ){
         break;
       }
-      blob_append(&text, zUtf8, -1);
-      fossil_mbcs_free(zUtf8);
+      blob_append(&text, zIn, -1);
     }
   }
   blob_remove_cr(&text);
