16afa92c8d7794419f96282635c9d89bfe61be10
mistachkin <mistachkin@noemail.net>
2013-04-29 23:44:07 +0000

    Fix x64 compilation on Windows using the MSVC toolset included with VS 2012.
    FossilOrigin-Name: ed544f3d9c2c734826def031bf8d281b69018581

--- win/include/dirent.h
+++ win/include/dirent.h
@@ -92,9 +92,13 @@
 #ifndef DIRENT_H
 #define DIRENT_H
 
-#if !defined(_68K_) && !defined(_MPPC_) && !defined(_X86_) && !defined(_IA64_) && !defined(_AMD64_) && defined(_M_IX86)
+#if !defined(_68K_) && !defined(_MPPC_) && !defined(_X86_) && !defined(_IA64_) && !defined(_AMD64_) && !defined(_ARM_) && defined(_M_IX86)
 #   define _X86_
 #endif
+#if !defined(_68K_) && !defined(_MPPC_) && !defined(_X86_) && !defined(_IA64_) && !defined(_AMD64_) && !defined(_ARM_) && defined(_M_AMD64)
+#define _AMD64_
+#endif
+
 #include <stdio.h>
 #include <stdarg.h>
 #include <windef.h>
