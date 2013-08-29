502c8703f37d64a00e4e841cc4d2403ffe69fb43
drh <drh@noemail.net>
2013-08-29 13:53:34 +0000

    Update the built-in SQLite to the version 3.8.0.1 beta.
    FossilOrigin-Name: 5d60e609c2320cd59cde967ceda2b056a12463fd

--- src/sqlite3.h
+++ src/sqlite3.h
@@ -107,9 +107,9 @@ extern "C" {
 ** [sqlite3_libversion_number()], [sqlite3_sourceid()],
 ** [sqlite_version()] and [sqlite_source_id()].
 */
-#define SQLITE_VERSION        "3.8.0"
+#define SQLITE_VERSION        "3.8.0.1"
 #define SQLITE_VERSION_NUMBER 3008000
-#define SQLITE_SOURCE_ID      "2013-08-22 02:56:28 0775501acf152dcbf4dd039f4511f3d8c4330d85"
+#define SQLITE_SOURCE_ID      "2013-08-29 13:47:05 c5857808c0707baa30994dd6aa3b9c93a74c0073"
 
 /*
 ** CAPI3REF: Run-Time Library Version Numbers
