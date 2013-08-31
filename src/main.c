da36de84634bb75dce43bf252687085bcf4c6adf
drh <drh@noemail.net>
2013-08-31 18:27:39 +0000

    Disable the test for file descriptor 3 being invalid since that messes up the "fossil diff" command.
    FossilOrigin-Name: fd7cfac508519fa98f52080022d61da3b68324bc

--- src/main.c
+++ src/main.c
@@ -612,7 +612,7 @@ int main(int argc, char **argv)
   }
 #ifndef _WIN32
   if( !is_valid_fd(2) ) fossil_panic("file descriptor 2 not open");
-  if( is_valid_fd(3) ) fossil_warning("file descriptor 3 is open");
+  /* if( is_valid_fd(3) ) fossil_warning("file descriptor 3 is open"); */
 #endif
   rc = name_search(zCmdName, aCommand, count(aCommand), &idx);
   if( rc==1 ){
