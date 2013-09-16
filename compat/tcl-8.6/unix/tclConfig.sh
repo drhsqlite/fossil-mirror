9dce9b41e34cc5dfbac069932a8cf5f3c23abee3
jan.nijtmans <jan.nijtmans@noemail.net>
2013-09-16 12:13:21 +0000

    Same stuff, but now for non-Windows systems as well. Need to run "make" in compat/tcl-8.6/unix first, before running "./configure"
    FossilOrigin-Name: 648c27e6ee093015477c8b72761240048a4eeb0d

---
+++ compat/tcl-8.6/unix/tclConfig.sh
@@ -0,0 +1,8 @@
+TCL_VERSION='8.6'
+TCL_PATCH_LEVEL=''
+TCL_PREFIX='compat/tcl-8.6/unix'
+TCL_LD_FLAGS=' '
+TCL_LIB_SPEC='-Lcompat/tcl-8.6/unix -ltcl8.6'
+TCL_INCLUDE_SPEC='-Icompat/tcl-8.6/generic'
+TCL_SUPPORTS_STUBS=1
+TCL_STUB_LIB_SPEC='-Lcompat/tcl-8.6/unix -ltclstub8.6'
