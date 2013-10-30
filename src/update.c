245c7a93f2fe985160fbb1ecdb2c20826cc4b5d2
jan.nijtmans <jan.nijtmans@noemail.net>
2013-10-30 09:11:30 +0000

    Make "fossil add", "fossil addremove", "fossil update" and "fossil co --latest" work when the repository doesn't contain any commit yet. This is not a fatal situation at all.
    FossilOrigin-Name: b725c1cf266b766626c4874d4d5a20978b57ca2e

--- src/update.c
+++ src/update.c
@@ -127,9 +127,6 @@ void update_cmd(void){
   capture_case_sensitive_option();
   db_must_be_within_tree();
   vid = db_lget_int("checkout", 0);
-  if( vid==0 ){
-    fossil_fatal("cannot find current version");
-  }
   user_select();
   if( !dryRunFlag && !internalUpdate ){
     autosync(SYNC_PULL + SYNC_VERBOSE*verboseFlag);
@@ -202,7 +199,7 @@ void update_cmd(void){
   }
 
   if( tid==0 ){
-    fossil_panic("unable to find a version to update to.");
+    return;
   }
 
   db_begin_transaction();
