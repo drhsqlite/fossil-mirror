704f4cdbccb499a5b4efd0e23ed350a5234f3b29
jan.nijtmans <jan.nijtmans@noemail.net>
2013-11-02 10:57:54 +0000

    Only prompt for password save if the to-be-saved password is different from the already saved password
    FossilOrigin-Name: a5404e30cbb4759efcbdba2313dc89ee2c721b0e

--- src/user.c
+++ src/user.c
@@ -137,6 +137,10 @@ void prompt_for_password(
 int save_password_prompt(){
   Blob x;
   char c;
+  const char *old = unobscure(db_get("last-sync-pw", 0));
+  if( (old!=0) && !strcmp(old, g.urlPasswd) ){
+     return 0;
+  }
   prompt_user("remember password (Y/n)? ", &x);
   c = blob_str(&x)[0];
   blob_reset(&x);
