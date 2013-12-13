9f22d46f977d2f469267b60765f4ecd900bcb5ea
jan.nijtmans <jan.nijtmans@noemail.net>
2013-12-13 13:48:07 +0000

    Better use GetCurrentDirectoryW in stead of _wgetcwd.
    FossilOrigin-Name: 4e463bf7bac9958d3171c3e385d907c43a1bfd64

--- src/file.c
+++ src/file.c
@@ -946,7 +946,7 @@ void file_getcwd(char *zBuf, int nBuf){
   int nPwd;
   int i;
   wchar_t zPwd[2000];
-  if( _wgetcwd(zPwd, sizeof(zPwd)/sizeof(zPwd[0])-1)==0 ){
+  if( GetCurrentDirectoryW(count(zPwd), zPwd)==0 ){
     fossil_fatal("cannot find the current working directory.");
   }
   zPwdUtf8 = fossil_filename_to_utf8(zPwd);
