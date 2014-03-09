1e62d09aa8b92c6d7cab995ba6547e202c196139
drh <drh@noemail.net>
2014-03-09 00:19:30 +0000

    Show the repository name on the test_env webpage.
    FossilOrigin-Name: 117a0f6952909f9f287ee6b3f109c3c245cade49

--- src/style.c
+++ src/style.c
@@ -1302,7 +1302,8 @@ void page_test_env(void){
   @ g.zLogin = %h(g.zLogin)<br />
   @ g.isHuman = %d(g.isHuman)<br />
   @ capabilities = %s(zCap)<br />
-  @ host-load-avg = %f(load_average())<br />
+  @ g.zRepositoryName = %h(g.zRepositoryName)<br />
+  @ load_average() = %f(load_average())<br />
   @ <hr>
   P("HTTP_USER_AGENT");
   cgi_print_all(showAll);
