39f8858f2c321811fab4c52256e087defdc81299
drh <drh@noemail.net>
2014-06-04 18:45:14 +0000

    Update the built-in SQLite to the 3.8.5 release.
    FossilOrigin-Name: 72328ae75d037030cfa7c2e7122c2f8f713df59e

--- src/sqlite3.h
+++ src/sqlite3.h
@@ -109,7 +109,7 @@ extern "C" {
 */
 #define SQLITE_VERSION        "3.8.5"
 #define SQLITE_VERSION_NUMBER 3008005
-#define SQLITE_SOURCE_ID      "2014-06-03 14:42:34 6d81805298ea882f5fee99504d5a95f1a762a90b"
+#define SQLITE_SOURCE_ID      "2014-06-04 14:06:34 b1ed4f2a34ba66c29b130f8d13e9092758019212"
 
 /*
 ** CAPI3REF: Run-Time Library Version Numbers
