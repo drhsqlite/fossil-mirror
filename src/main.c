7e61a78108c6bd11ad9020fbff5ee54e08a09801
drh <drh@noemail.net>
2014-10-30 22:08:00 +0000

    Fix an instance of using a NULL database connection pointer when starting the Fossil server in multi-repository mode.
    FossilOrigin-Name: de17e35bf12a8585f5c151c8b4ab3f8d36b6f0c8

--- src/main.c
+++ src/main.c
@@ -2202,7 +2202,7 @@ void cmd_webserver(void){
     }
     if( g.repositoryOpen ) flags |= HTTP_SERVER_HAD_REPOSITORY;
     if( g.localOpen ) flags |= HTTP_SERVER_HAD_CHECKOUT;
-  }else{
+  }else if( g.db ){
     db_setup_server_and_project_codes(1);
   }
   db_close(1);
