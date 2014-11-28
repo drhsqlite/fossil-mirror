e1cc3b8d75ad3b98258528c30ffdc01ac4a8a413
drh <drh@noemail.net>
2014-11-28 14:29:49 +0000

    Enhance the database aux-schema version checking so that it will accept a range of schema versions.  Fix the current implementation to work with or without the PLINK.BASEID enhancement.
    FossilOrigin-Name: 0d4b33d3a410d9f754db7858566e5c085bd22c77

--- src/manifest.c
+++ src/manifest.c
@@ -1785,10 +1785,20 @@ int manifest_crosslink(int rid, Blob *pContent, int flags){
       }
       for(i=0; i<p->nParent; i++){
         int pid = uuid_to_rid(p->azParent[i], 1);
-        db_multi_exec(
-           "INSERT OR IGNORE INTO plink(pid, cid, isprim, mtime, baseid)"
-           "VALUES(%d, %d, %d, %.17g, %s)",
-           pid, rid, i==0, p->rDate, zBaseId/*safe-for-%s*/);
+        if( strcmp(g.zAuxSchema,"2014-11-24 20:35")>=0 ){
+          /* Support for PLINK.BASEID added on 2014-11-24 */
+          db_multi_exec(
+             "INSERT OR IGNORE INTO plink(pid, cid, isprim, mtime, baseid)"
+             "VALUES(%d, %d, %d, %.17g, %s)",
+             pid, rid, i==0, p->rDate, zBaseId/*safe-for-%s*/);
+        }else{
+          /* Continue to work with older schema to avoid an unnecessary
+          ** rebuild */
+          db_multi_exec(
+             "INSERT OR IGNORE INTO plink(pid, cid, isprim, mtime)"
+             "VALUES(%d, %d, %d, %.17g)",
+             pid, rid, i==0, p->rDate);
+        }
         if( i==0 ){
           add_mlink(pid, 0, rid, p);
           parentid = pid;
