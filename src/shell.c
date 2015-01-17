69c1f82a9b8fe7947a116edc0112f0be8de9df0e
jan.nijtmans <jan.nijtmans@noemail.net>
2015-01-17 15:52:23 +0000

    Put back code from shell.c < 3.8.7, needed for .explain mode: The SQLITE_TESTCTRL_EXPLAIN_STMT test_control became a nop then, but is still needed when linking with older SQLite (./configure --disable-internal-sqlite).
    FossilOrigin-Name: 21acf839cb954e676c710ef71f1a5f5e6be8215c

--- src/shell.c
+++ src/shell.c
@@ -1473,6 +1473,17 @@ static int shell_exec(
         sqlite3_free(zEQP);
       }
 
+#if USE_SYSTEM_SQLITE+0==1
+      /* Output TESTCTRL_EXPLAIN text of requested */
+      if( pArg && pArg->mode==MODE_Explain && sqlite3_libversion_number()<3008007 ){
+        const char *zExplain = 0;
+        sqlite3_test_control(SQLITE_TESTCTRL_EXPLAIN_STMT, pStmt, &zExplain);
+        if( zExplain && zExplain[0] ){
+          fprintf(pArg->out, "%s", zExplain);
+        }
+      }
+#endif
+
       /* If the shell is currently in ".explain" mode, gather the extra
       ** data required to add indents to the output.*/
       if( pArg && pArg->mode==MODE_Explain ){
