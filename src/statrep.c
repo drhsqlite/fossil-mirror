2b5bc58a81642b7d72584a7bbf68ac0b4673e4a0
drh <drh@noemail.net>
2015-01-27 03:06:01 +0000

    Fix a stray "@" character in the filenames of the Checkins By File report.
    FossilOrigin-Name: 811d10ac7ad84e6b38d5abdbc854cf7f60604776

--- src/statrep.c
+++ src/statrep.c
@@ -492,7 +492,7 @@ static void stats_report_by_file(){
     sz = (int)(100*n/mxEvent);
     if( sz==0 ) sz = 1;
     @<tr class='row%d(++nRowNumber%2)'>
-    @ <td>@ %z(href("%R/finfo?name=%T",zFile))%h(zFile)</a></td>
+    @ <td>%z(href("%R/finfo?name=%T",zFile))%h(zFile)</a></td>
     @ <td>%d(n)</td>
     @ <td>
     @ <div class='statistics-report-graph-line'
