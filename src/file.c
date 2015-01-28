200a522bd2313251c98bb75576406dedc3ad6d81
jan.nijtmans <jan.nijtmans@noemail.net>
2015-01-28 03:33:59 +0000

    Fix for Cygwin ACL bug
    FossilOrigin-Name: 4043d5ea0b767c10ee6007eae013b170d9802b2c

--- src/file.c
+++ src/file.c
@@ -249,8 +249,7 @@ int file_wd_perm(const char *zFilename){
   else
     return PERM_REG;
 #else
-  if( S_ISREG(fileStat.st_mode) &&
-      ((S_IXUSR|S_IXGRP|S_IXOTH)&fileStat.st_mode)!=0 )
+  if( S_ISREG(fileStat.st_mode) && ((S_IXUSR)&fileStat.st_mode)!=0 )
     return PERM_EXE;
   else if( g.allowSymlinks && S_ISLNK(fileStat.st_mode) )
     return PERM_LNK;
@@ -434,12 +433,12 @@ int file_wd_setexe(const char *zFilename, int onoff){
   if( fossil_stat(zFilename, &buf, 1)!=0 || S_ISLNK(buf.st_mode) ) return 0;
   if( onoff ){
     int targetMode = (buf.st_mode & 0444)>>2;
-    if( (buf.st_mode & 0111)!=targetMode ){
+    if( (buf.st_mode & 0100) == 0 ){
       chmod(zFilename, buf.st_mode | targetMode);
       rc = 1;
     }
   }else{
-    if( (buf.st_mode & 0111)!=0 ){
+    if( (buf.st_mode & 0100) != 0 ){
       chmod(zFilename, buf.st_mode & ~0111);
       rc = 1;
     }
