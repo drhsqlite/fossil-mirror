36af9474a09477b392c113a09ff31680a61cd34f
drh <drh@noemail.net>
2015-02-14 19:04:35 +0000

    Updates to the /sitemap page.
    FossilOrigin-Name: 767c97c603633a82e4858beaf22b92a1b8394229

--- src/sitemap.c
+++ src/sitemap.c
@@ -36,46 +36,57 @@ void sitemap_page(void){
   @ </p>
   @
   @ <ul>
-  @ <li>%z(href("%R/home"))Home Page</a></li>
+  @ <li>%z(href("%R/home"))Home Page</a>
+  @   <ul>
+  @   <li>%z(href("%R/docsrc"))Search Project Documentation</a></li>
+  @   </ul></li>
   @ <li>%z(href("%R/tree"))File Browser</a></li>
-  @ <ul>
-  @   <li>%z(href("%R/tree?ci=trunk"))Tree-view, Trunk Checkin</a></li>
+  @   <ul>
+  @   <li>%z(href("%R/tree?type=tree&ci=trunk"))Tree-view,
+  @        Trunk Checkin</a></li>
   @   <li>%z(href("%R/tree?type=flat"))Flat-view</a></li>
   @   <li>%z(href("%R/fileage?name=trunk"))File ages for Trunk</a></li>
   @ </ul>
   @ <li>%z(href("%R/timeline?n=200"))Project Timeline</a></li>
   @ <ul>
-  @   <li>%z(href("%R/timeline?a=1970-01-01&y=ci&n=10"))First 10 checkins</a></li>
+  @   <li>%z(href("%R/timeline?a=1970-01-01&y=ci&n=10"))First 10 
+  @        checkins</a></li>
   @   <li>%z(href("%R/timeline?n=all&namechng"))All checkins with file name
   @        changes</a></li>
   @   <li>%z(href("%R/reports"))Activity Reports</a></li>
   @ </ul>
-  @ <li>Branches and Tags</a>
+  @ <li>%z(href("%R/brlist"))Branches</a></li>
   @ <ul>
-  @   <li>%z(href("%R/brlist"))Branches</a></li>
   @   <li>%z(href("%R/leaves"))Leaf Checkins</a></li>
   @   <li>%z(href("%R/taglist"))List of Tags</a></li>
   @ </ul>
   @ </li>
-  @ <li>%z(href("%R/wiki"))Wiki</a>
+  @ <li>%z(href("%R/wikihelp"))Wiki</a>
   @   <ul>
+  @     <li>%z(href("%R/wikisrch"))Wiki Search</a></li>
   @     <li>%z(href("%R/wcontent"))List of Wiki Pages</a></li>
   @     <li>%z(href("%R/timeline?y=w"))Recent activity</a></li>
   @     <li>%z(href("%R/wiki_rules"))Wiki Formatting Rules</a></li>
+  @     <li>%z(href("%R/md_rules"))Markdown Formatting Rules</a></li>
+  @     <li>%z(href("%R/wiki?name=Sandbox"))Sandbox</a></li>
   @     <li>%z(href("%R/attachlist"))List of Attachments</a></li>
   @   </ul>
   @ </li>
   @ <li>%z(href("%R/reportlist"))Tickets</a>
   @   <ul>
+  @   <li>%z(href("%R/tktsrch"))Ticket Search</a></li>
   @   <li>%z(href("%R/timeline?y=t"))Recent activity</a></li>
   @   <li>%z(href("%R/attachlist"))List of Attachments</a></li>
   @   </ul>
   @ </li>
+  @ <li>%z(href("%R/search"))Full-Text Search</a></li>
   @ <li>%z(href("%R/login"))Login/Logout/Change Password</a></li>
-  @ <li>Repository Status
+  @ <li>%z(href("%R/stat"))Repository Status</a>
   @   <ul>
-  @   <li>%z(href("%R/stat"))Status Summary</a></li>
-  @   <li>%z(href("%R/urllist"))List of URLs used to access this repository</a></li>
+  @   <li>%z(href("%R/hash-collisions"))Collisions on SHA1 hash
+  @       prefixes</a></li>
+  @   <li>%z(href("%R/urllist"))List of URLs used to access
+  @       this repository</a></li>
   @   <li>%z(href("%R/bloblist"))List of Artifacts</a></li>
   @   </ul></li>
   @ <li>On-line Documentation
@@ -83,11 +94,11 @@ void sitemap_page(void){
   @   <li>%z(href("%R/help"))List of All Commands and Web Pages</a></li>
   @   <li>%z(href("%R/test-all-help"))All "help" text on a single page</a></li>
   @   </ul></li>
-  @ <li>Administration Pages
+  @ <li>%z(href("%R/setup"))Administration Pages</a>
   @   <ul>
-  @   <li>%z(href("%R/setup"))Configuration and Setup Menu</a></li>
   @   <li>%z(href("%R/modreq"))Pending Moderation Requests</a></li>
   @   <li>%z(href("%R/admin_log"))Admin log</a></li>
+  @   <li>%z(href("%R/cachestat"))Status of the web-page cache</a></li>
   @   </ul></li>
   @ <li>Test Pages
   @   <ul>
