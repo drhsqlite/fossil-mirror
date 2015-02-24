e189cdc78e6326ec1b6f8287d02c5b31408e8b7b
jan.nijtmans <jan.nijtmans@noemail.net>
2015-02-24 06:40:00 +0000

    Let -x imply --emptydirs and --dotfiles
    FossilOrigin-Name: 8c3e6404b096b3be13e848b655a7f2c4cad61270

--- src/checkin.c
+++ src/checkin.c
@@ -604,8 +604,8 @@ void clean_cmd(void){
   extremeFlag = find_option("extreme","x",0)!=0;
   allFileFlag = allDirFlag = find_option("force","f",0)!=0;
   dirsOnlyFlag = find_option("dirsonly",0,0)!=0;
-  emptyDirsFlag = find_option("emptydirs","d",0)!=0 || dirsOnlyFlag;
-  if( find_option("dotfiles",0,0)!=0 ) scanFlags |= SCAN_ALL;
+  emptyDirsFlag = find_option("emptydirs","d",0)!=0 || dirsOnlyFlag || extremeFlag;
+  if( find_option("dotfiles",0,0)!=0 || extremeFlag) scanFlags |= SCAN_ALL;
   if( find_option("temp",0,0)!=0 ) scanFlags |= SCAN_TEMP;
   if( find_option("allckouts",0,0)!=0 ) scanFlags |= SCAN_NESTED;
   zIgnoreFlag = find_option("ignore",0,1);
