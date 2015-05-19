e3cfa7f9af3b7c7846f380bc653f136c3443cb55
mistachkin <mistachkin@noemail.net>
2015-05-19 19:22:08 +0000

    Revert [48499514cc], changing nUsed to zero is not redundant as blob_append() changes it to one.
    FossilOrigin-Name: 4742716fc16c713eb296c29902c0267ee432844f

--- src/blob.c
+++ src/blob.c
@@ -302,7 +302,8 @@ void blob_copy(Blob *pTo, Blob *pFrom){
 char *blob_str(Blob *p){
   blob_is_init(p);
   if( p->nUsed==0 ){
-    blob_append(p, "", 1);
+    blob_append(p, "", 1); /* NOTE: Changes nUsed. */
+    p->nUsed = 0;
   }
   if( p->aData[p->nUsed]!=0 ){
     blob_materialize(p);
