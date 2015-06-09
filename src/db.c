67fcaf148ae0456cfc200a13eab565825d060dff
jan.nijtmans <jan.nijtmans@noemail.net>
2015-06-09 10:56:05 +0000

    Add "diff-cmd-abs-paths" setting, controlling whether "fossil diff/gdiff" provide absolute paths to the external diff tool or not.
    FossilOrigin-Name: 55b4a751c8816348b21e2f96a21dc9596f0c3ec6

--- src/db.c
+++ src/db.c
@@ -2375,6 +2375,11 @@ const Setting aSetting[] = {
   { "crnl-glob",        0,             40, 1, 0, ""                    },
   { "default-perms",    0,             16, 0, 0, "u"                   },
   { "diff-binary",      0,              0, 0, 0, "on"                  },
+#if defined(_WIN32) || defined(__CYGWIN__)
+  { "diff-cmd-abs-path",0,              0, 0, 0, "off"                 },
+#else
+  { "diff-cmd-abs-path",0,              0, 0, 0, "on"                  },
+#endif
   { "diff-command",     0,             40, 0, 0, ""                    },
   { "dont-push",        0,              0, 0, 0, "off"                 },
   { "dotfiles",         0,              0, 1, 0, "off"                 },
