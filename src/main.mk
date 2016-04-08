b929a27d930f709410f8ec9d39e78161d36ad5dc
drh <drh@noemail.net>
2016-04-08 14:47:04 +0000

    Add the SQLITE_SHELL_IS_UTF8 command-line option on windows in order to fix the build.
    FossilOrigin-Name: a233ed2168f63f6d80f25831e22c2b901bc8c401

--- src/main.mk
+++ src/main.mk
@@ -496,6 +496,7 @@ SQLITE_OPTIONS = -DNDEBUG=1 \
 
 # Setup the options used to compile the included SQLite shell.
 SHELL_OPTIONS = -Dmain=sqlite3_shell \
+                -DSQLITE_SHELL_IS_UTF8=1 \
                 -DSQLITE_OMIT_LOAD_EXTENSION=1 \
                 -DUSE_SYSTEM_SQLITE=$(USE_SYSTEM_SQLITE) \
                 -DSQLITE_SHELL_DBNAME_PROC=fossil_open
