00001b7cbb916092cda5a33f144c7d6f21a2d8a1
drh <drh@noemail.net>
2016-10-02 00:45:59 +0000

    Update the built-in SQLite to the latest 3.15.0 beta for testing. Add new performance-enhancing compile-time options to the SQLite build.
    FossilOrigin-Name: dfe32f53acc2c1dfe5a07983d9308133d8c7c233

--- src/sqlite3.h
+++ src/sqlite3.h
@@ -123,7 +123,7 @@ extern "C" {
 */
 #define SQLITE_VERSION        "3.15.0"
 #define SQLITE_VERSION_NUMBER 3015000
-#define SQLITE_SOURCE_ID      "2016-09-28 16:05:53 40c0fb0af678797c39a99853f9f4102464c16f4b"
+#define SQLITE_SOURCE_ID      "2016-10-01 23:55:23 fe07609efc31c4639c40fbda501c55f443756ad2"
 
 /*
 ** CAPI3REF: Run-Time Library Version Numbers
