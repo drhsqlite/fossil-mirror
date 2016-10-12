0e81c234c540bcb2635091f8fcc8166acd118f22
andygoth <andygoth@noemail.net>
2016-10-12 03:45:13 +0000

    Revise proposed changes options and help text.
    FossilOrigin-Name: 69b41682655375ff1ca2c2a31051bcad8bba749a

--- src/checkin.c
+++ src/checkin.c
@@ -208,42 +208,51 @@ void print_changes(
 ** Each line of output is the name of a changed file, with paths shown
 ** according to the "relative-paths" setting, unless overridden by the
 ** --abs-paths or --rel-paths options.
-** 
+**
 ** By default, all changed files are selected for display.  This behavior
 ** can be overridden by using one or more filter options (listed below),
 ** in which case only files with the specified change type(s) are shown.
+** As a special case, the --no-merge option does not inhibit this default.
 **
-** If file change type classification is enabled, each output line starts
-** with a code describing the file's change type, e.g. EDITED or RENAMED.
-** This can be turned on or off using the --classify or --no-classify
-** options, respectively.  By default, it is enabled only when more than
-** one change type is enabled (i.e. zero or multiple filter options or
-** --all are used) and disabled when only one change type is enabled.
+** If no filter options are used, or if the --merge option is used, the
+** SHA1 hash of each merge contributor check-in version is displayed at
+** the end of the report.  The --no-merge option is useful to display the
+** default set of changed files but not the merge contributors.
+**
+** If change type classification is enabled, each output line starts with
+** a code describing the file's change type, e.g. EDITED or RENAMED.  It
+** is enabled by default except when exactly one filter option (besides
+** --merge or --no-merge) is used.  The default can be overridden by the
+** --classify or --no-classify options.
 **
 ** The "fossil changes -extra" command is equivalent to "fossil extras".
 **
-** Options:
+** General options:
 **    --abs-paths       Display absolute pathnames.
 **    --rel-paths       Display pathnames relative to the current working
 **                      directory.
 **    --sha1sum         Verify file status using SHA1 hashing rather than
 **                      relying on file mtimes.
-**    --header          Identify the repository if any files are listed.
-**    -v|--verbose      Say "(none)" if no files are listed.
+**    --header          Identify the repository if report is non-empty.
+**    -v|--verbose      Say "(none)" if the change report is empty.
 **    --classify        Start each line with the file's change type.
 **    --no-classify     Do not print file change types.
 **
 ** Filter options:
-**    --edited          Display edited files.
+**    --edited          Display edited, merged, and conflicted files.
+**    --updated         Display files updated by merge/integrate.
+**    --modified        Combination of the above two options.
 **    --missing         Display missing files.
 **    --added           Display added files.
 **    --deleted         Display deleted files.
 **    --renamed         Display renamed files.
-**    --conflict        Display merge conflict files.
-**    --unmodified      Display unmodified files.
+**    --conflict        Display files having merge conflicts.
 **    --meta            Display files with metadata changes.
+**    --unmodified      Display unmodified files.
 **    --all             Display all managed files, i.e. all of the above.
 **    --extra           Display unmanaged files.
+**    --merge           Display merge contributors.
+**    --no-merge        Do not display merge contributors.
 **
 ** See also: extras, ls, status
 */
