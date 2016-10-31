50a4dedd87855290eb2a6b8a186d831d33a83a6e
drh <drh@noemail.net>
2016-10-31 16:02:47 +0000

    Fix to the fusefs.c file so that it builds even without FOSSIL_HAVE_FUSEFS.
    FossilOrigin-Name: 9a4a5dec3d0a9a1ccb133518d6180e4e598eefad

--- src/fusefs.c
+++ src/fusefs.c
@@ -22,8 +22,9 @@
 ** The FOSSIL_HAVE_FUSEFS should be omitted on systems that lack support for
 ** the Fuse Filesystem, of course.
 */
-#ifdef FOSSIL_HAVE_FUSEFS
 #include "config.h"
+#include "fusefs.h"
+#ifdef FOSSIL_HAVE_FUSEFS
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
@@ -31,7 +32,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include "fusefs.h"
 
 #define FUSE_USE_VERSION 26
 #include <fuse.h>
@@ -285,6 +285,7 @@ static struct fuse_operations fusefs_methods = {
   .readdir = fusefs_readdir,
   .read    = fusefs_read,
 };
+#endif /* FOSSIL_HAVE_FUSEFS */
 
 /*
 ** COMMAND: fusefs
@@ -316,6 +317,9 @@ static struct fuse_operations fusefs_methods = {
 ** again.
 */
 void fusefs_cmd(void){
+#ifndef FOSSIL_HAVE_FUSEFS
+  fossil_fatal("fusefs not supported in this build");
+#else
   char *zMountPoint;
   char *azNewArgv[5];
   int doDebug = find_option("debug","d",0)!=0;
@@ -337,5 +341,5 @@ void fusefs_cmd(void){
   fuse_main(4, azNewArgv, &fusefs_methods, NULL);
   fusefs_reset();
   fusefs_clear_path();
+#endif
 }
-#endif /* FOSSIL_HAVE_FUSEFS */
