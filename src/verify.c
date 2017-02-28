68f19de579a94d9ffa93f59047b1023886b3895f
drh <drh@noemail.net>
2017-02-28 20:43:45 +0000

    Remove a reference to sha1sum from the pre-commit verification logic.
    FossilOrigin-Name: fc246fc4d1e9e98528b6af2f15f504cb585dd235

--- src/verify.c
+++ src/verify.c
@@ -46,13 +46,11 @@ static void verify_rid(int rid){
     fossil_fatal("not a valid rid: %d", rid);
   }
   if( content_get(rid, &content) ){
-    sha1sum_blob(&content, &hash);
-    blob_reset(&content);
-    if( blob_compare(&uuid, &hash) ){
-      fossil_fatal("hash of rid %d (%b) does not match its uuid (%b)",
-                    rid, &hash, &uuid);
+    if( !hname_verify_hash(&content, blob_buffer(&uuid), blob_size(&uuid)) ){
+      fossil_fatal("hash of rid %d does not match its uuid (%b)",
+                    rid, &uuid);
     }
-    blob_reset(&hash);
+    blob_reset(&content);
   }
   blob_reset(&uuid);
 }
