163455cdbb5aa32ec386472b4f4775d7172da718
joerg <joerg@noemail.net>
2017-04-18 18:40:46 +0000

    Index xmark(trid), otherwise writing the mark file can take hours.
    FossilOrigin-Name: 48d466ab73882c57bf50de59f05228ac7d7a2f41ccd6dbf00736918060849a67

--- src/export.c
+++ src/export.c
@@ -510,6 +510,7 @@ void export_cmd(void){
   db_multi_exec("CREATE TEMPORARY TABLE oldblob(rid INTEGER PRIMARY KEY)");
   db_multi_exec("CREATE TEMPORARY TABLE oldcommit(rid INTEGER PRIMARY KEY)");
   db_multi_exec("CREATE TEMP TABLE xmark(tname TEXT UNIQUE, trid INT, tuuid TEXT)");
+  db_multi_exec("CREATE INDEX xmark_trid ON xmark(trid)");
   if( markfile_in!=0 ){
     Stmt qb,qc;
     FILE *f;
