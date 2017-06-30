cde576d7c1458b0f5c6709ea0e298b3ebd65991b
drh <drh@noemail.net>
2017-06-30 15:17:56 +0000

    Start the security audit by checking to see if the repos it public or private.
    FossilOrigin-Name: 7f29e2640ab5d1dc7271dad859c608bcc335ff753ebd9eacc66501c31b3bc4c5

--- src/security_audit.c
+++ src/security_audit.c
@@ -22,6 +22,18 @@
 #include <assert.h>
 #include "security_audit.h"
 
+/*
+** Return TRUE if any of the capability letters in zTest are found
+** in the capability string zCap.
+*/
+static int hasAnyCap(const char *zCap, const char *zTest){
+  while( zTest[0] ){
+    if( strchr(zCap, zTest[0]) ) return 1;
+    zTest++;
+  }
+  return 0;
+}
+
 
 /*
 ** WEBPAGE: secaudit0
@@ -30,12 +42,102 @@
 ** This page requires administrator access
 */
 void secaudit0_page(void){
+  const char *zAnonCap;      /* Capabilities of user "anonymous" and "nobody" */
+  const char *zPubPages;     /* GLOB pattern for public pages */
+
   login_check_credentials();
   if( !g.perm.Setup && !g.perm.Admin ){
     login_needed(0);
     return;
   }
   style_header("Security Audit");
-  @ Under Construction...
+  @ <ol>
+
+  /* Step 1:  Determine if the repository is public or private.  "Public"
+  ** means that any anonymous user on the internet can access all content.
+  ** "Private" repos require (non-anonymous) login to access all content,
+  ** though some content may be accessible anonymously.
+  */
+  zAnonCap = db_text("", "SELECT group_concat(coalesce(cap,'')) FROM user"
+                         " WHERE login IN ('anonymous','nobody')");
+  zPubPages = db_get("public-pages",0);
+  if( hasAnyCap(zAnonCap,"as") ){
+    @ <li><p>This repository is <big><b>Wildly INSECURE</b></big> because
+    @ it grants administrator privileges to anonymous users.  You
+    @ should <a href="takeitprivate">take this repository private</a>
+    @ immediately!  Or, at least remove the Setup and Admin privileges
+    @ for users "anonymous" and "login" on the
+    @ <a href="setup_ulist">User Configuration</a> page.
+  }else if( hasAnyCap(zAnonCap,"y") ){
+    @ <li><p>This repository is <big><b>INSECURE</b></big> because
+    @ it allows anonymous users to push unversioned files.  You can
+    @ fix this by <a href="takeitprivate">taking the repository private</a>
+    @ or by removing the "y" permission from users "anonymous" and
+    @ "nobody" on the <a href="setup_ulist">User Configuration</a> page.
+  }else if( hasAnyCap(zAnonCap,"goz") ){
+    @ <li><p>This repository is <big><b>PUBLIC</b></big>. All
+    @ checked-in content can be accessed by anonymous passers-by on the
+    @ internet.  <a href="takeitprivate">Take it private</a>.<p>
+  }else if( !hasAnyCap(zAnonCap, "jry") && (zPubPages==0 || zPubPages[0]==0) ){
+    @ <li><p>This repository is <big><b>Completely PRIVATE</b></big>.
+    @ A valid login and password is required to access any content.
+  }else{
+    @ <li><p>This repository is <big><b>Mostly PRIVATE</b></big>.
+    @ A valid login and password is usually required, however some
+    @ content can be accessed anonymously:
+    @ <ul>
+    if( zPubPages && zPubPages[0] ){
+      @ <li> URLs that matches one of these GLOB patterns:
+      @ "%h(zPubPages)" (Change this using the "Public pages" setting
+      @ on the <a href="setup_access">Access Settings</a> page.)
+    }
+    if( hasAnyCap(zAnonCap,"j") ){
+      @ <li> Wiki pages
+    }
+    if( hasAnyCap(zAnonCap,"r") ){
+      @ <li> Tickets
+    }
+    @ </ul>
+  }
+  
+  style_footer();
+}
+
+/*
+** WEBPAGE: takeitprivate
+**
+** Disable anonymous access to this website
+*/
+void takeitprivate_page(void){
+  login_check_credentials();
+  if( !g.perm.Setup && !g.perm.Admin ){
+    login_needed(0);
+    return;
+  }
+  if( P("cancel") ){
+    /* User pressed the cancel button.  Go back */
+    cgi_redirect("secaudit0");
+  }
+  if( P("apply") ){
+    db_multi_exec(
+      "UPDATE user SET cap=''"
+      " WHERE login IN ('nobody','anonymous');"
+      "DELETE FROM config WHERE name='public-pages';"
+    );
+    cgi_redirect("secaudit0");
+  }
+  style_header("Make This Website Private");
+  @ <p>Click the "Make It Private" button below to disable all
+  @ anonymous access to this repository.  A valid login and password
+  @ will be required to access this repository after clicking that
+  @ button.</p>
+  @
+  @ <p>Click the "Cancel" button to leave things as they are.</p>
+  @
+  @ <form action="%s(g.zPath)" method="post">
+  @ <input type="submit" name="apply" value="Make It Private">
+  @ <input type="submit" name="cancel" value="Cancel">
+  @ </form>
+
   style_footer();
 }
