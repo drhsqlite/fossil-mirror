bb150d3ae2360f7e52211a18a30393a575e61d78
drh <drh@noemail.net>
2018-04-10 17:48:28 +0000

    Update the built-in SQLite to version 3.23.1.
    FossilOrigin-Name: 52b3b8ed223190c52501f19850a307e45665665e43598ed64655b9d391f82f94

--- src/shell.c
+++ src/shell.c
@@ -11257,7 +11257,6 @@ static FILE *output_file_open(const char *zFile, int bTextMode){
   return f;
 }
 
-#if !defined(SQLITE_UNTESTABLE)
 #if !defined(SQLITE_OMIT_TRACE) && !defined(SQLITE_OMIT_FLOATING_POINT)
 /*
 ** A routine for handling output from sqlite3_trace().
@@ -11279,7 +11278,6 @@ static int sql_trace_callback(
   }
   return 0;
 }
-#endif
 #endif
 
 /*
