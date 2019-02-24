71e2205cc3e4cba2eb16570bd1bac74bbb02acf3
mistachkin <mistachkin@noemail.net>
2019-02-24 20:32:06 +0000

    Update error message in 'loadTcl' to make it more concise.
    FossilOrigin-Name: f2f01669d01bd99642ee1843037345157c64f79c383fc6f1c00240644a278b6b

--- src/th_tcl.c
+++ src/th_tcl.c
@@ -974,7 +974,7 @@ static int loadTcl(
   } while( --aFileName[TCL_MINOR_OFFSET]>'3' ); /* Tcl 8.4+ */
   aFileName[TCL_MINOR_OFFSET] = 'x';
   Th_ErrorMessage(interp,
-      "could not load any supported Tcl 8.6, 8.5, or 8.4 shared library \"",
+      "could not load any supported Tcl 8.x shared library \"",
       aFileName, -1);
   return TH_ERROR;
 #else
