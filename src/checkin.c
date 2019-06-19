af7d675ad388c753fcb4cebd7131da6a50086402
wyoung <wyoung@noemail.net>
2019-06-19 18:24:01 +0000

    Added a link to https://fossil-scm.org/fossil/doc/trunk/www/branching.wiki#branching from the recent changes to the fossil ci "would fork" error message. *Hopefully* this will end the debate.
    FossilOrigin-Name: b761a72922ab2b02855a6226685191c4dcfdc114b8184a1ba2155409680a8d16

--- src/checkin.c
+++ src/checkin.c
@@ -2274,14 +2274,20 @@ void commit_cmd(void){
     if( sCiInfo.zBranch==0 ){
       if( allowFork==0 && forceFlag==0 && g.markPrivate==0
         && db_exists("SELECT 1 from event where type='ci'") ){
-        fossil_fatal("would fork.  \"update\" first or use --branch.");
+        fossil_fatal("would fork.  \"update\" first or use --branch.  See also\n"
+          "https://fossil-scm.org/fossil/doc/trunk/www/branching.wiki#branching");
       }
       sCiInfo.zBranch = db_get("main-branch", "trunk");
     }
   }else if( sCiInfo.zBranch==0 && allowFork==0 && forceFlag==0
     && g.markPrivate==0 && !is_a_leaf(vid)
   ){
-    fossil_fatal("would fork.  \"update\" first or use --branch.");
+    /* Can't avoid duplicating this string because some C compilers
+    ** refuse to see static const char zErr[] = "... as "constant"
+    ** enough for a printf() style format string.  (e.g. Clang 10)
+    */
+    fossil_fatal("would fork.  \"update\" first or use --branch.  See also\n"
+      "https://fossil-scm.org/fossil/doc/trunk/www/branching.wiki#branching");
   }
 
   /*
