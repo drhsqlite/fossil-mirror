ba529cbd0af2cba608c26068db886a7d14fda959
stephan <stephan@noemail.net>
2019-07-02 07:31:50 +0000

    When activating one of the new "in reply to" links, tag the newly-focused post with CSS class forumSelReplyTo so that it can be visually marked. Added default forumSelReplyTo CSS class which uses the same border as forumTime but with a dotted style.
    FossilOrigin-Name: 1bf7985d109f3b3cee67bf55bd46c21aa82fcbf6b260b631b8ee0c3b8e96ca1b

--- src/forum.js
+++ src/forum.js
@@ -1,4 +1,5 @@
 (function(){
+  "use strict";
   function absoluteY(obj){
     var top = 0;
     if( obj.offsetParent ){
@@ -16,4 +17,32 @@
     if( w>h ) y = y + (h-w)/2;
     if( y>0 ) window.scrollTo(0, y);
   }
-})()
+
+  // Set up click handlers for "in reply to" links...
+  var respondeeSelectClass = 'forumSelReplyTo'
+  /* CSS class to apply to selected "in reply to" post. */;
+  var responseLinkClick = function(){
+    /** This <A> tag has an href in the form /something#post-{UID},
+        and post-{UID} is the ID of a forum post DIV on this
+        page. Here we fish that ID out of this anchor,
+        unmark any currently-selected DIV, and mark this anchor's
+        corresponding DIV.
+    */
+    var m = /#post-\w+/.exec(this.href);
+    if(!m || !m.length) return /*unexpected*/;
+    // Remove respondeeSelectClass from all entries...
+    document.querySelectorAll('.forumPost.'+respondeeSelectClass)
+      .forEach(function(e){
+        e.classList.remove(respondeeSelectClass);
+      });
+    // Add respondeeSelectClass to the matching entry...
+    document.querySelectorAll(m[0])
+      .forEach(function(e){
+        e.classList.add(respondeeSelectClass);
+      });
+  };
+  document.querySelectorAll('a[href*="#post-"]')
+    .forEach(function(e){
+      e.addEventListener( "click", responseLinkClick, false );
+    });
+})();
