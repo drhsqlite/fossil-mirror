8254174e4792ab84fdf0927e6f3355727f0ef78b
drh <drh@noemail.net>
2019-08-07 12:49:23 +0000

    Fix an incorrect comment in sqlcompattest.c.
    FossilOrigin-Name: e2426e83e417c1120d22d5f2a3ef2e941499ab7d1e6591e80da2e6b6ba141528

--- src/sqlcompattest.c
+++ src/sqlcompattest.c
@@ -44,10 +44,10 @@
 int main(int argc, char **argv){
   int i;
   static const char *zRequiredOpts[] = {
-    "ENABLE_FTS4",         /* Required for repository search */
-    "ENABLE_JSON1",        /* Required for the check-in locking protocol */
-    "ENABLE_DBSTAT_VTAB",  /* Required by /repo-tabsize page */
-    "ENABLE_DBPAGE_VTAB",  /* Required by the .dump command in "fossil sql" */
+    "ENABLE_FTS4",        /* Required for repository search */
+    "ENABLE_JSON1",       /* Required for the check-in locking protocol */
+    "ENABLE_DBSTAT_VTAB", /* Required by /repo-tabsize page */
+    "ENABLE_DBPAGE_VTAB", /* Required by the .recover command in "fossil sql" */
   };
 
   /* Check minimum SQLite version number */
