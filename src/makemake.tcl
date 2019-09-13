feb240798a9e436d5a57d40399dec3fa15cf410d
jan.nijtmans <jan.nijtmans@noemail.net>
2019-09-13 22:53:09 +0000

    Update to openssl 1.1.1d
    FossilOrigin-Name: 74aac0ed3d6e8ccb096c2a3f4d5895649e2b363560419e717296d734ece41d08

--- src/makemake.tcl
+++ src/makemake.tcl
@@ -715,7 +715,7 @@ endif
 #    to create a hard link between an "openssl-1.x" sub-directory of the
 #    Fossil source code directory and the target OpenSSL source directory.
 #
-OPENSSLDIR = $(SRCDIR)/../compat/openssl-1.1.1c
+OPENSSLDIR = $(SRCDIR)/../compat/openssl-1.1.1d
 OPENSSLINCDIR = $(OPENSSLDIR)/include
 OPENSSLLIBDIR = $(OPENSSLDIR)
 
@@ -1572,7 +1572,7 @@ USE_SEE = 0
 !endif
 
 !if $(FOSSIL_ENABLE_SSL)!=0
-SSLDIR    = $(B)\compat\openssl-1.1.1c
+SSLDIR    = $(B)\compat\openssl-1.1.1d
 SSLINCDIR = $(SSLDIR)\include
 !if $(FOSSIL_DYNAMIC_BUILD)!=0
 SSLLIBDIR = $(SSLDIR)
