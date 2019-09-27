c3b563592d3fb3d127aaf3157df2b83a70f4f85f
stephan <stephan@noemail.net>
2019-09-27 08:48:57 +0000

    Initial work on unified "--" flag support, as requested in https://fossil-scm.org/forum/forumpost/64acc6b653. There's still lots to do here.
    FossilOrigin-Name: d8ebbd76cc9ae94b6f9ffb8ef6856b3afd5ee2b5a14063d923fd567206f08ad3

--- src/main.c
+++ src/main.c
@@ -902,12 +902,24 @@ static void remove_from_argv(int i, int n){
 
 
 /*
-** Look for a command-line option.  If present, return a pointer.
-** Return NULL if missing.
+** Look for a command-line option.  If present, remove it from the
+** argument list and return a pointer to either the flag's name (if
+** hasArg==0), sans leading - or --, or its value (if hasArg==1).
+** Return NULL if the flag is not found.
+**
+** zLong is the "long" form of the flag and zShort is the
+** short/abbreviated form (typically a single letter, but it may be
+** longer). zLong must not be NULL, but zShort may be.
 **
 ** hasArg==0 means the option is a flag.  It is either present or not.
-** hasArg==1 means the option has an argument.  Return a pointer to the
-** argument.
+** hasArg==1 means the option has an argument, in which case a pointer
+** to the argument's value is returned. For zLong, a flag value (if
+** hasValue==1) may either be in the form (--flag=value) or (--flag
+** value). For zShort, only the latter form is accepted.
+**
+** If a standalone argument of "--" is encountered in the argument
+** list while searching for the given flag(s), this routine stops
+** searching and NULL is returned.
 */
 const char *find_option(const char *zLong, const char *zShort, int hasArg){
   int i;
@@ -923,7 +935,7 @@ const char *find_option(const char *zLong, const char *zShort, int hasArg){
     z++;
     if( z[0]=='-' ){
       if( z[1]==0 ){
-        remove_from_argv(i, 1);
+        /* Stop processing at "--" */
         break;
       }
       z++;
@@ -957,7 +969,13 @@ int has_option(const char *zOption){
     char *z = g.argv[i];
     if( z[0]!='-' ) continue;
     z++;
-    if( z[0]=='-' ) z++;
+    if( z[0]=='-' ){
+      z++;
+      if( z[0]==0 ){
+        /* Stop searching at "--" */
+        break;
+      }
+    }
     if( strncmp(z,zOption,n)==0 && (z[n]==0 || z[n]=='=') ) return 1;
   }
   return 0;
@@ -971,7 +989,10 @@ int has_option(const char *zOption){
 **
 ** pnUsedArgs is used to store the number of matched arguments.
 **
-** Caller is responsible to free allocated memory.
+** Caller is responsible for freeing allocated memory by passing the
+** head of the array (not each entry) to fossil_free(). (The
+** individual entries have the same lifetime as values returned from
+** find_option().)
 */
 const char **find_repeatable_option(
   const char *zLong,
@@ -1015,18 +1036,48 @@ const char *find_repository_option(){
 ** Verify that there are no unprocessed command-line options.  If
 ** Any remaining command-line argument begins with "-" print
 ** an error message and quit.
+**
+** If fAllowDoubleDash is true then if the flag "--" is found, it is
+** removed from the list and arguments after that flag are not
+** inspected by this function (they are assumed to be filenames, even
+** if they syntactically look like flags). If fAllowDoubleDash is
+** false then the "--" flag will trigger a fatal error exactly as if
+** an unprocessed flag were encountered.
+**
+** Sidebar: the question of whether fAllowDoubleDash should be true or
+** false would seem to boil down to: does the calling routine
+** expect/allow arbitrary file/page/branch/whatever name arguments
+** after its required arguments?
+**
+** Once the "--" support is completed, this function will be renamed
+** (back) to verify_all_options().
 */
-void verify_all_options(void){
+void verify_all_options_porting_crutch(int fAllowDoubleDash){
   int i;
   for(i=1; i<g.argc; i++){
-    if( g.argv[i][0]=='-' && g.argv[i][1]!=0 ){
-      fossil_fatal(
-        "unrecognized command-line option, or missing argument: %s",
-        g.argv[i]);
+    const char * arg = g.argv[i];
+    if( arg[0]=='-' ){
+      if( arg[1]=='-' && arg[2]==0 ){
+        if(fAllowDoubleDash){
+          /* Remove "--" from the list and assume any following
+          ** arguments are file names. */
+          remove_from_argv(i, 1);
+          break;
+        }else{
+          fossil_fatal("The -- flag is not allowed here.");
+        }
+      }else if( arg[1]!=0 ){
+        fossil_fatal(
+          "unrecognized command-line option, or missing argument: %s",
+          arg);
+      }
     }
   }
 }
 
+void verify_all_options(void){
+  verify_all_options_porting_crutch(0);
+}
 
 /*
 ** This function returns a human readable version string.
