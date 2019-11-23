5651f2c6c9d1a8937bfe1dbf1212d75e3b1ffcd1
andygoth <andygoth@noemail.net>
2019-11-23 20:54:30 +0000

    Restore operation of "related" and "tag filter" widgets, see forum thread TBD for discussion
    FossilOrigin-Name: e2581e6db794781e4baf35ae21de86bdd7f59a4fc61f06d0ffafba59d6c1499a

--- src/timeline.c
+++ src/timeline.c
@@ -1684,8 +1684,11 @@ void page_timeline(void){
     );
   }
 
-  /* r=TAG works like a combination of t=TAG & rel */
+  /* Convert r=TAG to t=TAG&rel. */
   if( zBrName && !related ){
+    cgi_delete_query_parameter("r");
+    cgi_set_query_parameter("t", zBrName);
+    cgi_set_query_parameter("rel", "1");
     zTagName = zBrName;
     related = 1;
     zType = "ci";
