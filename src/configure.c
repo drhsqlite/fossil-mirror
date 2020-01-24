dd56fd5c0a0f4d2af68d42cfdc7fdb1a4ffc60d1
stephan <stephan@noemail.net>
2020-01-24 18:56:15 +0000

    Added mimetypes setting to the 'project' configuration area, per forum feedback.
    FossilOrigin-Name: 7391f8d4863a85a4fd656ac5d12812184042c7d40406081be7396b293c477a7f

--- src/configure.c
+++ src/configure.c
@@ -145,6 +145,7 @@ static struct {
   { "parent-project-name",    CONFIGSET_PROJ },
   { "hash-policy",            CONFIGSET_PROJ },
   { "comment-format",         CONFIGSET_PROJ },
+  { "mimetypes",              CONFIGSET_PROJ },
 
 #ifdef FOSSIL_ENABLE_LEGACY_MV_RM
   { "mv-rm-files",            CONFIGSET_PROJ },
