9df4755a7d70769abfa5941978f8c2d0e9ace1f2
stephan <stephan@noemail.net>
2020-05-12 13:18:45 +0000

    Added fossil.fetch() responseHeaders option to pass on one or more response headers to the onload() callback.
    FossilOrigin-Name: bbb738d07dfbecb9f3f5923765fb6c1cf66fb301bf36ec4bfc3ba69782a7b46c

--- src/fossil.fetch.js
+++ src/fossil.fetch.js
@@ -42,13 +42,26 @@
    "blob", or "document") (as specified by XHR2). Default = "text".
    As an extension, it supports "json", which tells it that the
    response is expected to be text and that it should be JSON.parse()d
-   before passing it on to the onload() callback.
+   before passing it on to the onload() callback. If parsing of such
+   an object fails, the onload callback is not called, and the
+   onerror() callback is passed the exception from the parsing error.
 
    - urlParams: string|object. If a string, it is assumed to be a
    URI-encoded list of params in the form "key1=val1&key2=val2...",
    with NO leading '?'.  If it is an object, all of its properties get
    converted to that form. Either way, the parameters get appended to
    the URL before submitting the request.
+
+   - responseHeaders: If true, the onload() callback is passed an
+   additional argument: a map of all of the response headers. If it's
+   a string value, the 2nd argument passed to onload() is instead the
+   value of that single header. If it's an array, it's treated as a
+   list of headers to return, and the 2nd argument is a map of those
+   header values. When a map is passed on, all of its keys are
+   lower-cased. When a single header is requested and that header is
+   set multiple times, they are (per the XHR docs) concatenated
+   together with ", " between them.
+
 
    When an options object does not provide onload() or onerror()
    handlers of its own, this function falls back to
@@ -63,7 +76,7 @@
 */
 window.fossil.fetch = function f(uri,opt){
   const F = fossil;
-  if(!f.onerror){
+  if(!f.onerror){/* "static" functions... */
     f.onerror = function(e/*event or exception*/){
       console.error("Ajax error:",e);
       if(e instanceof Error){
@@ -85,7 +98,19 @@ window.fossil.fetch = function f(uri,opt){
       }
     };
     f.onload = (r)=>console.debug('ajax response:',r);
-  }
+    f.parseResponseHeaders = function(h){
+      const rc = {};
+      if(!h) return rc;
+      const ar = h.trim().split(/[\r\n]+/);
+      ar.forEach(function(line) {
+        const parts = line.split(': ');
+        const header = parts.shift();
+        const value = parts.join(': ');
+        rc[header.toLowerCase()] = value;
+      });
+      return rc;
+    };
+  }/*static init*/
   if('/'===uri[0]) uri = uri.substr(1);
   if(!opt) opt = {};
   else if('function'===typeof opt) opt={onload:opt};
@@ -120,20 +145,32 @@ window.fossil.fetch = function f(uri,opt){
   }else{
     x.responseType = opt.responseType||'text';
   }
-  if(opt.onload){
-    x.onload = function(e){
-      if(200!==this.status){
-        if(opt.onerror) opt.onerror(e);
-        return;
-      }
-      try{
-        opt.onload((jsonResponse && this.response)
-                   ? JSON.parse(this.response) : this.response);
-      }catch(e){
-        if(opt.onerror) opt.onerror(e);
-      }
+  x.onload = function(e){
+    if(200!==this.status){
+      if(opt.onerror) opt.onerror(e);
+      return;
     }
-  }
+    const orh = opt.responseHeaders;
+    let head;
+    if(true===orh){
+      head = f.parseResponseHeaders(this.getAllResponseHeaders());
+    }else if('string'===typeof orh){
+      head = this.getResponseHeader(orh);
+    }else if(orh instanceof Array){
+      head = {};
+      orh.forEach((s)=>{
+        if('string' === typeof s) head[s.toLowerCase()] = x.getResponseHeader(s);
+      });
+    }
+    try{
+      const args = [(jsonResponse && this.response)
+                    ? JSON.parse(this.response) : this.response];
+      if(head) args.push(head);
+      opt.onload.apply(opt, args);
+    }catch(e){
+      if(opt.onerror) opt.onerror(e);
+    }
+  };
   if(undefined!==payload) x.send(payload);
   else x.send();
   return this;
