82133b5fdf3d6ec2a2dcce6d2b9ef24b1f10712e
stephan <stephan@noemail.net>
2020-08-14 17:57:25 +0000

    Several forum style improvements based on forum feedback.
    FossilOrigin-Name: 2121b1f7a765114b7a50092617e1a87937ddbaab75a3ff2ab64da2545013eaa1

--- src/fossil.page.forumpost.js
+++ src/fossil.page.forumpost.js
@@ -15,26 +15,51 @@
         contentElem.classList.toggle('expanded');
         if(wasExpanded){
           contentElem.classList.add('shrunken');
-          contentElem.parentElement.scrollIntoView();
+          contentElem.parentElement.scrollIntoView({
+            /* This is non-standard, but !(MSIE, Safari) supposedly support it:
+               https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView#Browser_compatibility
+            */ behavior: 'smooth'
+          });
         }else{
           contentElem.classList.remove('shrunken');
         }
         return false;
       };
     };
+
     /* Adds an Expand/Collapse toggle to all div.forumPostBody
        elements which are deemed "too large" (those for which
        scrolling is currently activated because they are taller than
        their max-height). */
     document.querySelectorAll(
       'div.forumHier, div.forumTime, div.forumHierRoot'
-    ).forEach(function(forumPostWrapper){
+    ).forEach(function f(forumPostWrapper){
       const content = forumPostWrapper.querySelector('div.forumPostBody');
       if(!content || !scrollbarIsVisible(content)) return;
       const parent = content.parentElement,
             rightTapZone = D.div(),
-            widget = D.div(),
-            widgetEventHandler = getWidgetHandler(widget, content);
+            widget = D.div();
+      /* Repopulates the rightTapZone with arrow indicators. Because
+         of the wildly varying height of these elements, This has to
+         be done dynamically at init time and upon collapse/expand. Will not
+         work until the rightTapZone has been added to the DOM. */
+      const refillTapZone = function f(){
+        if(!f.baseTapIndicatorHeight){
+          /* To figure out how often to place an arrow in the rightTapZone,
+             we simply grab the first header element from the page and use
+             its hight as our basis for calculation. */
+          const h1 = document.querySelector('h1, h2');
+          f.baseTapIndicatorHeight = h1.getBoundingClientRect().height;
+        }
+        D.clearElement(rightTapZone);
+        var rtzHeight = parseInt(window.getComputedStyle(rightTapZone).height);
+        do {
+          D.append(rightTapZone, D.span());
+          rtzHeight -= f.baseTapIndicatorHeight * 8;
+        }while(rtzHeight>0);
+      };
+      const handlerStep1 = getWidgetHandler(widget, content);
+      const widgetEventHandler = ()=>{ handlerStep1(); refillTapZone(); };
       content.classList.add('with-expander');
       widget.classList.add('forum-post-collapser');
       widget.addEventListener('click', widgetEventHandler, false);
@@ -48,13 +73,9 @@
       }else{
         forumPostWrapper.appendChild(widget);
       }
-      /** A double-click toggle will select "the current word" on the
-          post, which is minorly annoying but otherwise harmless. Such
-          a toggle has proven convenient on "excessive" posts,
-          though. */
-      //content.addEventListener('dblclick', widgetEventHandler);
       content.appendChild(rightTapZone);
       rightTapZone.addEventListener('click', widgetEventHandler, false);
+      refillTapZone();
     });
   })/*onload callback*/;
   
