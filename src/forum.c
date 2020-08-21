2dfc34f3af58c993fa1cac96f6e2da99d3f902f1
andygoth <andygoth@noemail.net>
2020-08-21 20:19:52 +0000

    Add new fields to threadtable debug output
    FossilOrigin-Name: c4d6d757168b01c7f41322b77fefcd7a4513750143e1f2b2c892e9d2ac282bc9

--- src/forum.c
+++ src/forum.c
@@ -523,12 +523,16 @@ static void forum_display_chronological(int froot, int target, int bRawMode){
   if( PB("threadtable") ){
     @ <hr>
     @ <table border="1" cellpadding="3" cellspacing="0">
-    @ <tr><th>sid<th>fpid<th>firt<th>fprev<th>mfirt<th>pEditTail<th>nReply\
-    @     <th>hash
+    @ <tr><th>sid<th>fpid<th>firt<th>fprev<th>mfirt<th>pEditHead<th>pEditTail\
+    @     <th>pEditNext<th>pEditPrev<th>nReply<th>hash
     for(p=pThread->pFirst; p; p=p->pNext){
       @ <tr><td>%d(p->sid)<td>%d(p->fpid)<td>%d(p->firt)\
       @ <td>%d(p->fprev)<td>%d(p->mfirt)\
-      @ <td>%d(p->pEditTail?p->pEditTail->fpid:0)<td>%d(p->nReply)\
+      @ <td>%d(p->pEditHead?p->pEditHead->fpid:0)\
+      @ <td>%d(p->pEditTail?p->pEditTail->fpid:0)\
+      @ <td>%d(p->pEditNext?p->pEditNext->fpid:0)\
+      @ <td>%d(p->pEditPrev?p->pEditPrev->fpid:0)\
+      @ <td>%d(p->nReply)\
       @ <td>%S(p->zUuid)</tr>
     }
     @ </table>
