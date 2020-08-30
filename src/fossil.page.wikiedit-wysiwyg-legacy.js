776c0618d9c06dc49584b461fefcea86f95fa5c8
stephan <stephan@noemail.net>
2020-08-30 13:20:45 +0000

    Significant cleanups in and shortening of the legacy wysiwyg JS code.
    FossilOrigin-Name: b8297d34bd3e0093bdce049770fec7de24afaf833c0474442fff1d75706cedaa

--- src/fossil.page.wikiedit-wysiwyg-legacy.js
+++ src/fossil.page.wikiedit-wysiwyg-legacy.js
@@ -4,11 +4,6 @@
    API.
 
    Requires: window.fossil, fossil.dom.
-
-   Maintenance note: mkbuiltins strips anything which looks like a
-   C++-style comment, even if it's in a string literal, and thus the
-   runs of / characters in DOM element data attributes have been
-   mangled to work around that.
 */
 (function(F/*fossil object*/){
   'use strict';
@@ -22,26 +17,44 @@
           styleCSS = `
 .intLink { cursor: pointer; }
 img.intLink { border: 0; }
+#wysiwyg-container {
+  max-width: calc(100% - 1em);
+  display: flex;
+  flex-direction: column;
+}
 #wysiwygBox {
   border: 1px #000000 solid;
-  padding: 12px;
-}
-#wysiwygEditMode label { cursor: pointer; }
-#wysiwygBox {
-  resize: body;
+  padding: 0 1em;
   overflow: auto;
-  width: calc(100% - 1em);
   min-height: 20em;
 }
+#wysiwygEditMode label { cursor: pointer; }
 #wysiwyg-toolbars {
+  margin: 0 0 0.25em 0;
   display: flex;
+  flex-wrap: wrap;
   flex-direction: row;
+  align-items: flex-start;
+}
+#wysiwyg-toolbars > * {
+  margin: 0 0.5em 0.25em 0;
+  align-self: center;
+}
+#wysiwyg-toolBar1, #wysiwyg-toolBar2 {
+}
+#wysiwyg-toolBar1 > * { /* formatting buttons */
+  margin: 0 0.2em 0.2em 0;
+}
+#wysiwyg-toolBar2 > * { /* icons */
+  margin: 0 0.2em 0.2em 0;
+  border: 1px solid rgba(127,127,127,0.3);
+  align-self: end;
 }
 `;
     head.appendChild(styleTag);
     /* Adapted from https://stackoverflow.com/a/524721 */
     styleTag.type = 'text/css';
-    styleTag.appendChild(document.createTextNode(styleCSS));
+    D.append(styleTag, styleCSS);
   })();
 
   const outerContainer = D.attr(D.div(), 'id', 'wysiwyg-container');
@@ -49,22 +62,23 @@ img.intLink { border: 0; }
   const toolbars = D.attr(D.div(), 'id', 'wysiwyg-toolbars');
   D.append(outerContainer, toolbars);
 
-  var select, div = D.attr(D.div(), 'id', 'wysiwygEditModeDiv');
+  var select, div;
+
+  const toolbar1 = D.attr(D.div(), 'id', 'wysiwyg-toolBar1');
+
   const selectEditMode = select = D.attr(
     D.attr(D.select(), 'id', 'wysiwygEditMode'),
     'size',
     1
   );
-  D.append(
-    toolbars,
-    D.append(div, select)
-  );
+  D.append(toolbar1, select);
   D.option(select, "0", "WYSIWYG");
   D.option(select, "1", "Raw HTML");
   select.selectedIndex = 0;
 
-  const toolbar1 = div = D.attr(D.div(), 'id', 'toolBar1');
-  D.append(toolbars, toolbar1);
+  
+  div = toolbar1;
+  D.append(toolbars, ' ', toolbar1);
   select = D.addClass(D.select(), 'format');
   select.dataset.format = "formatblock";
   D.append(div, select);
@@ -116,42 +130,44 @@ img.intLink { border: 0; }
   D.option(select, "blue", "Blue");
   D.option(select, "green", "Green");
   D.option(select, "black", "Black");
+  D.option(select, "yellow", "Yellow");
+  D.option(select, "cyan", "Cyan");
+  D.option(select, "magenta", "Magenta");
 
-  const toolbar2 = div = D.attr(D.div(), 'id', 'toolBar2');
+  const toolbar2 = D.attr(D.div(), 'id', 'wysiwyg-toolBar2');
   D.append(toolbars, toolbar2);
-  var img = D.img();
-  D.append(div, img);
-  D.addClass(img, 'intLink');
-  D.attr(img, 'title', 'Undo');
-  img.dataset.format = 'undo';
-  D.attr(
-    img, 'src',
+
+  /**
+     Inject the icons...
+
+     mkbuiltins strips anything which looks like a C++-style comment,
+     even if it's in a string literal, and thus the runs of "/"
+     characters in the DOM element data attributes have been mangled
+     to work around that: we simply use \u002f for every 2nd slash.
+  */
+  (function f(title,format,src){
+    const img = D.img();
+    D.append(toolbar2, ' ', img);
+    D.addClass(img, 'intLink');
+    D.attr(img, 'title', title);
+    img.dataset.format = format;
+    D.attr(img, 'src', 'string'===typeof src ? src : src.join(''));
+    return f;
+  })(
+    'Undo', 'undo',
     ["data:image/gif;base64,R0lGODlhFgAWAOMKADljwliE33mOrpGjuYKl8aezxqPD+7",
      "/I19DV3NHa7P/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f",
      "/\u002f/\u002f/\u002f/yH5BAEKAA8ALAAAAAAWABYAAARR8MlJq704680",
      "7TkaYeJJBnES4EeUJvIGapWYAC0CsocQ7SDlWJkAkCA6ToMYWIARGQF3mRQVIEjkkSVLIbSfE",
-     "whdRIH4fh/DZMICe3/C4nBQBADs="].join('')
-  );
-
-  img = D.img();
-  D.append(div, img);
-  D.addClass(img, "intLink");
-  D.attr(img, 'title', "Redo");
-  img.dataset.format="redo";
-  D.attr(
-    img, 'src',
+     "whdRIH4fh/DZMICe3/C4nBQBADs="]
+  )(
+    'Redo','redo',
     ["data:image/gif;base64,R0lGODlhFgAWAMIHAB1ChDljwl9vj1iE34Kl8aPD+7/I1/",
      "/\u002f/yH5BAEKAAcALAAAAAAWABYAAANKeLrc/jDKSesyphi7SiEgsVXZEATDICqBVJjpqWZt9Na",
-     "EDNbQK1wCQsxlYnxMAImhyDoFAElJasRRvAZVRqqQXUy7Cgx4TC6bswkAOw=="].join('')
-  );
-
-  img = D.img();
-  D.append(div, img);
-  D.addClass(img, "intLink");
-  D.attr(img, 'title', "Remove formatting");
-  img.dataset.format="removeFormat";
-  D.attr(
-    img, 'src',
+     "EDNbQK1wCQsxlYnxMAImhyDoFAElJasRRvAZVRqqQXUy7Cgx4TC6bswkAOw=="]
+  )(
+    "Remove formatting",
+    "removeFormat",
     ["data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAYAAADEtGw7AA",
      "AABGdBTUEAALGPC/xhBQAAAAZiS0dEAP8A/wD/oL2nkwAAAAlwSFlzAAAOxAAADsQBlSsOGwA",
      "AAAd0SU1FB9oECQMCKPI8CIIAAAAIdEVYdENvbW1lbnQA9syWvwAAAuhJREFUOMtjYBgFxAB5",
@@ -168,197 +184,106 @@ img.intLink { border: 0; }
      "b/mZm0JcT5Lj+89+Ybm6zz95oMh7s4XbygN3Sluq4Mj5K8iKMgP4f0/\u002f/\u002ffv77/\u002f8nLy+7MCc",
      "XmyYDAwODS9jM9tcvPypd35pne3ljdjvj26+H2dhYpuENikgfvQeXNmSl3tqepxXsqhXPyc66",
      "6s+fv1fMdKR3TK72zpix8nTc7bdfhfkEeVbC9KhbK/9iYWHiErbu6MWbY/7/\u002f8/4/\u002f9/pgOnH",
-     "6jGVazvFDRtq2VgiBIZrUTIBgCk+ivHvuEKwAAAAABJRU5ErkJggg=="].join('')
-  );
-
-  img = D.img();
-  D.append(div, img);
-  D.addClass(img, "intLink");
-  D.attr(img, 'title', "Bold");
-  img.dataset.format="bold";
-  D.attr(
-    img, 'src',
+     "6jGVazvFDRtq2VgiBIZrUTIBgCk+ivHvuEKwAAAAABJRU5ErkJggg=="]
+  )(
+    "Bold",
+    "bold",
     ["data:image/gif;base64,R0lGODlhFgAWAID/AMDAwAAAACH5BAEAAAAALAAAAAAWAB",
-     "YAQAInhI+pa+H9mJy0LhdgtrxzDG5WGFVk6aXqyk6Y9kXvKKNuLbb6zgMFADs="].join('')
-  );
-
-  img = D.img();
-  D.append(div, img);
-  D.addClass(img, "intLink");
-  D.attr(img, 'title', "Italic");
-  img.dataset.format="italic";
-  D.attr(
-    img, 'src',
+     "YAQAInhI+pa+H9mJy0LhdgtrxzDG5WGFVk6aXqyk6Y9kXvKKNuLbb6zgMFADs="]
+  )(
+    "Italic",
+    "italic",
     ["data:image/gif;base64,R0lGODlhFgAWAKEDAAAAAF9vj5WIbf/\u002f/yH5BAEAAAMALA",
-     "AAAAAWABYAAAIjnI+py+0Po5x0gXvruEKHrF2BB1YiCWgbMFIYpsbyTNd2UwAAOw=="].join('')
-  );
-
-  img = D.img();
-  D.append(div, img);
-  D.addClass(img, "intLink");
-  D.attr(img, 'title', "Underline");
-  img.dataset.format="underline";
-  D.attr(
-    img, 'src',
+     "AAAAAWABYAAAIjnI+py+0Po5x0gXvruEKHrF2BB1YiCWgbMFIYpsbyTNd2UwAAOw=="]
+  )(
+    "Underline",
+    "underline",
     ["data:image/gif;base64,R0lGODlhFgAWAKECAAAAAF9vj/\u002f/\u002f/\u002f/\u002fyH5BAEAAAIALA",
      "AAAAAWABYAAAIrlI+py+0Po5zUgAsEzvEeL4Ea15EiJJ5PSqJmuwKBEKgxVuXWtun+DwxCCgA",
-     "7"].join('')
-  );
-
-  img = D.img();
-  D.append(div, img);
-  D.addClass(img, "intLink");
-  D.attr(img, 'title', "Left align");
-  img.dataset.format="justifyleft";
-  D.attr(
-    img, 'src',
+     "7"]
+  )(
+    "Left align",
+    "justifyleft",
     ["data:image/gif;base64,R0lGODlhFgAWAID/AMDAwAAAACH5BAEAAAAALAAAAAAWAB",
-     "YAQAIghI+py+0Po5y02ouz3jL4D4JMGELkGYxo+qzl4nKyXAAAOw=="].join('')
-  );
-
-  img = D.img();
-  D.append(div, img);
-  D.addClass(img, "intLink");
-  D.attr(img, 'title', "Center align");
-  img.dataset.format="justifycenter";
-  D.attr(
-    img, 'src',
+     "YAQAIghI+py+0Po5y02ouz3jL4D4JMGELkGYxo+qzl4nKyXAAAOw=="]
+  )(
+    "Center align",
+    "justifycenter",
     ["data:image/gif;base64,R0lGODlhFgAWAID/AMDAwAAAACH5BAEAAAAALAAAAAAWAB",
-     "YAQAIfhI+py+0Po5y02ouz3jL4D4JOGI7kaZ5Bqn4sycVbAQA7"].join('')
-  );
-
-  img = D.img();
-  D.append(div, img);
-  D.addClass(img, "intLink");
-  D.attr(img, 'title', "Right align");
-  img.dataset.format="justifyright";
-  D.attr(
-    img, 'src',
+     "YAQAIfhI+py+0Po5y02ouz3jL4D4JOGI7kaZ5Bqn4sycVbAQA7"]
+  )(
+    "Right align",
+    "justifyright",
     ["data:image/gif;base64,R0lGODlhFgAWAID/AMDAwAAAACH5BAEAAAAALAAAAAAWAB",
-     "YAQAIghI+py+0Po5y02ouz3jL4D4JQGDLkGYxouqzl43JyVgAAOw=="].join('')
-  );
-
-  img = D.img();
-  D.append(div, img);
-  D.addClass(img, "intLink");
-  D.attr(img, 'title', "Numbered list");
-  img.dataset.format="insertorderedlist";
-  D.attr(
-    img, 'src',
+     "YAQAIghI+py+0Po5y02ouz3jL4D4JQGDLkGYxouqzl43JyVgAAOw=="]
+  )(
+    "Numbered list",
+    "insertorderedlist",
     ["data:image/gif;base64,R0lGODlhFgAWAMIGAAAAADljwliE35GjuaezxtHa7P/\u002f/\u002f",
      "/\u002f/yH5BAEAAAcALAAAAAAWABYAAAM2eLrc/jDKSespwjoRFvggCBUBoTFBeq6QIAysQnRHaEO",
-     "zyaZ07Lu9lUBnC0UGQU1K52s6n5oEADs="].join('')
-  );
-
-  img = D.img();
-  D.append(div, img);
-  D.addClass(img, "intLink");
-  D.attr(img, 'title', "Dotted list");
-  img.dataset.format="insertunorderedlist";
-
-  D.attr(
-    img, 'src',
+     "zyaZ07Lu9lUBnC0UGQU1K52s6n5oEADs="]
+  )(
+    "Dotted list",
+    "insertunorderedlist",
     ["data:image/gif;base64,R0lGODlhFgAWAMIGAAAAAB1ChF9vj1iE33mOrqezxv/\u002f/\u002f",
      "/\u002f/yH5BAEAAAcALAAAAAAWABYAAAMyeLrc/jDKSesppNhGRlBAKIZRERBbqm6YtnbfMY7lud6",
-     "4UwiuKnigGQliQuWOyKQykgAAOw=="].join('')
-  );
-
-  img = D.img();
-  D.append(div, img);
-  D.addClass(img, "intLink");
-  D.attr(img, 'title', "Quote");
-  img.dataset.format="formatblock";
-  D.attr(
-    img, 'src',
+     "4UwiuKnigGQliQuWOyKQykgAAOw=="]
+  )(
+    "Quote",
+    "formatblock",
     ["data:image/gif;base64,R0lGODlhFgAWAIQXAC1NqjFRjkBgmT9nqUJnsk9xrFJ7u2",
      "R9qmKBt1iGzHmOrm6Sz4OXw3Odz4Cl2ZSnw6KxyqO306K63bG70bTB0rDI3bvI4P",
      "/\u002f/\u002f/\u002f/\u002f/",
      "/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f",
      "/\u002f/\u002f/\u002fyH5BAEKAB8ALAAAAAAWABYAAAVP4CeOZGmeaKqubEs2Cekk",
      "ErvEI1zZuOgYFlakECEZFi0GgTGKEBATFmJAVXweVOoKEQgABB9IQDCmrLpjETrQQlhHjINrT",
-     "q/b7/i8fp8PAQA7"].join('')
-  );
-
-  img = D.img();
-  D.append(div, img);
-  D.addClass(img, "intLink");
-  D.attr(img, 'title', "Delete indentation");
-  img.dataset.format="outdent";
-  D.attr(
-    img, 'src',
+     "q/b7/i8fp8PAQA7"]
+  )(
+    "Delete indentation",
+    "outdent",
     ["data:image/gif;base64,R0lGODlhFgAWAMIHAAAAADljwliE35GjuaezxtDV3NHa7P",
      "/\u002f/yH5BAEAAAcALAAAAAAWABYAAAM2eLrc/jDKCQG9F2i7u8agQgyK1z2EIBil+TWqEMxhMcz",
-     "sYVJ3e4ahk+sFnAgtxSQDqWw6n5cEADs="].join('')
-  );
-
-  img = D.img();
-  D.append(div, img);
-  D.addClass(img, "intLink");
-  D.attr(img, 'title', "Add indentation");
-  img.dataset.format="indent";
-  D.attr(
-    img, 'src',
+     "sYVJ3e4ahk+sFnAgtxSQDqWw6n5cEADs="]
+  )(
+    "Add indentation",
+    "indent",
     ["data:image/gif;base64,R0lGODlhFgAWAOMIAAAAADljwl9vj1iE35GjuaezxtDV3N",
      "Ha7P/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/yH5BAEAAAgALAAAAAAWABYAAAQ7EMlJq704650",
      "B/x8gemMpgugwHJNZXodKsO5oqUOgo5KhBwWESyMQsCRDHu9VOyk5TM9zSpFSr9gsJwIAOw=="
-    ].join('')
-  );
-
-  img = D.img();
-  D.append(div, img);
-  D.addClass(img, "intLink");
-  D.attr(img, 'title', "Hyperlink");
-  img.dataset.format="createlink";
-  D.attr(
-    img, 'src',
+    ]
+  )(
+    "Hyperlink",
+    "createlink",
     ["data:image/gif;base64,R0lGODlhFgAWAOMKAB1ChDRLY19vj3mOrpGjuaezxrCztb",
      "/I19Ha7Pv8/f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/yH5BAEKAA8ALAAAAAAWABYAAARY8MlJq704682",
      "7/2BYIQVhHg9pEgVGIklyDEUBy/RlE4FQF4dCj2AQXAiJQDCWQCAEBwIioEMQBgSAFhDAGghG",
-     "i9XgHAhMNoSZgJkJei33UESv2+/4vD4TAQA7"].join('')
-  );
-
-  img = D.img();
-  D.append(div, img);
-  D.addClass(img, "intLink");
-  D.attr(img, 'title', "Cut");
-  img.dataset.format="cut";
-  D.attr(
-    img, 'src',
+     "i9XgHAhMNoSZgJkJei33UESv2+/4vD4TAQA7"]
+  )(
+    "Cut",
+    "cut",
     ["data:image/gif;base64,R0lGODlhFgAWAIQSAB1ChBFNsRJTySJYwjljwkxwl19vj1",
      "dusYODhl6MnHmOrpqbmpGjuaezxrCztcDCxL/I18rL1P/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/",
      "/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002fyH5BAEAAB8ALAAAAAAWABYAAAVu4CeOZGmeaKqubDs6TNnE",
      "bGNApNG0kbGMi5trwcA9GArXh+FAfBAw5UexUDAQESkRsfhJPwaH4YsEGAAJGisRGAQY7UCC9",
-     "ZAXBB+74LGCRxIEHwAHdWooDgGJcwpxDisQBQRjIgkDCVlfmZqbmiEAOw=="].join('')
-  );
-
-  img = D.img();
-  D.append(div, img);
-  D.addClass(img, "intLink");
-  D.attr(img, 'title', "Copy");
-  img.dataset.format="copy";
-  D.attr(
-    img, 'src',
+     "ZAXBB+74LGCRxIEHwAHdWooDgGJcwpxDisQBQRjIgkDCVlfmZqbmiEAOw=="]
+  )(
+    "Copy",
+    "copy",
     ["data:image/gif;base64,R0lGODlhFgAWAIQcAB1ChBFNsTRLYyJYwjljwl9vj1iE31",
      "iGzF6MnHWX9HOdz5GjuYCl2YKl8ZOt4qezxqK63aK/9KPD+7DI3b/I17LM/MrL1MLY9NHa7OP",
      "s++bx/Pv8/f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/yH5BAEAAB8ALAAAAAAWABYAAAWG4CeOZGmeaKqubOum1SQ/",
      "kPVOW749BeVSus2CgrCxHptLBbOQxCSNCCaF1GUqwQbBd0JGJAyGJJiobE+LnCaDcXAaEoxhQ",
      "ACgNw0FQx9kP+wmaRgYFBQNeAoGihCAJQsCkJAKOhgXEw8BLQYciooHf5o7EA+kC40qBKkAAA",
-     "Grpy+wsbKzIiEAOw=="].join('')
-  );
-
-  img = D.img();
-  D.append(div, img);
-  D.addClass(img, "intLink");
-  D.attr(img, 'title', "Paste");
-  img.dataset.format="paste";
-  D.attr(
-    img, 'src',
+     "Grpy+wsbKzIiEAOw=="]
+  )(
+    "Paste",
+    "paste",
     ["data:image/gif;base64,R0lGODlhFgAWAIQUAD04KTRLY2tXQF9vj414WZWIbXmOrp",
      "qbmpGjudClFaezxsa0cb/I1+3YitHa7PrkIPHvbuPs+/fvrvv8/f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/",
      "/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002f/\u002fyH5BAEAAB8ALAAAAAAWABYAAAWN4CeOZGmeaKqubGsusPvB",
      "SyFJjVDs6nJLB0khR4AkBCmfsCGBQAoCwjF5gwquVykSFbwZE+AwIBV0GhFog2EwIDchjwRiQ",
      "o9E2Fx4XD5R+B0DDAEnBXBhBhN2DgwDAQFjJYVhCQYRfgoIDGiQJAWTCQMRiwwMfgicnVcAAA",
-     "MOaK+bLAOrtLUyt7i5uiUhADs="].join('')
+     "MOaK+bLAOrtLUyt7i5uiUhADs="]
   );
 
   const oDoc = div = D.attr(D.div(), 'id', "wysiwygBox");
@@ -450,23 +375,28 @@ img.intLink { border: 0; }
     if(!setDocMode.linebreak){
       setDocMode.linebreak = new RegExp("</p><p>","ig");
     }
+    if(!setDocMode.toHide){
+      setDocMode.toHide = toolbars.querySelectorAll(
+        '#wysiwyg-toolBar1 > *:not(#wysiwygEditMode), '
+          +'#wysiwyg-toolBar2');
+    }
     if (bToMarkup) {
       /* WYSIWYG -> Markup */
       // Legacy did this: content=content.replace(setDocMode.linebreak,"</p>\n\n<p>")
       D.append(D.clearElement(oDoc), content)
       oDoc.style.whiteSpace = "pre-wrap";
-      D.addClass([toolbar1, toolbar2], 'hidden');
+      D.addClass(setDocMode.toHide, 'hidden');
     } else {
       /* Markup -> WYSIWYG */
       oDoc.innerHTML = content;
       oDoc.style.whiteSpace = "normal";
-      D.removeClass([toolbar1, toolbar2], 'hidden');
+      D.removeClass(setDocMode.toHide, 'hidden');
     }
     oDoc.focus();
   }
 
   F.page.wysiwyg = {
-    oDoc: oDoc,
+    // only for debugging: oDoc: oDoc,
     init: function(){
       initDoc();
       /* Must not be called outside of an onPageLoad handler, else it
