9686b245a9602b329cef26dcf39d5b59218bda33
stephan <stephan@noemail.net>
2020-09-14 02:06:13 +0000

    pikchrshow now supports stashing a single pikchr to/from browser-local storage to enable switching back and forth between example scripts while editing one's own script. Experimentally re-indented the C-side implementation to make its generated HTML hierarchy clearer. Over-the-wire size is now 4.8kb.
    FossilOrigin-Name: 49a04c97b5a78055f2da0f7920ba4c9a2a444bb22336a1296e91ebe9c9f7b590

--- src/pikchrshow.c
+++ src/pikchrshow.c
@@ -72,96 +72,104 @@ void pikchrshow_page(void){
       "box same \"Pikchr\" \"Formatter\" \"(pikchr.c)\" fit\n";
   }
   style_header("PikchrShow");
-  CX("<style>");
-  CX("div.content { padding-top: 0.5em }\n");
-  CX("#sbs-wrapper {"
-     "display: flex; flex-direction: column;"
-     "}\n");
-  CX("#sbs-wrapper > * {"
-     "margin: 0 0.25em 0.5em 0; flex: 1 10 auto;"
-     "align-self: stretch;"
-     "}\n");
-  CX("#sbs-wrapper textarea {"
-     "max-width: initial; flex: 1 1 auto;"
-     "}\n");
-  CX("#pikchrshow-output, #pikchrshow-form"
-     "{display: flex; flex-direction: column; align-items: stretch;}");
-  CX("#pikchrshow-form > * {margin: 0.25em 0}\n");
-  CX("#pikchrshow-output {flex: 5 1 auto; padding: 0}\n");
-  CX("#pikchrshow-output > pre, "
-     "#pikchrshow-output > pre > div, "
-     "#pikchrshow-output > pre > div > pre "
-     "{margin: 0; padding: 0}\n");
-  CX("#pikchrshow-output.error > pre "
-     /* Server-side error report */
-     "{padding: 0.5em}\n");
-  CX("#pikchrshow-controls {" /* where the buttons live */
-     "display: flex; flex-direction: row; "
-     "align-items: center; flex-wrap: wrap;"
-     "}\n");
-  CX("#pikchrshow-controls > * {"
-     "display: inline; margin: 0 0.25em 0.5em 0;"
-     "}\n");
-  CX("#pikchrshow-output-wrapper label {"
-     "cursor: pointer;"
-     "}\n");
-  CX("body.pikchrshow .input-with-label > * {"
-     "margin: 0 0.2em;"
-     "}\n");
-  CX("body.pikchrshow .input-with-label > label {"
-     "cursor: pointer;"
-     "}\n");
-  CX("#pikchrshow-output.dark-mode svg {"
-     /* Flip the colors to approximate a dark theme look */
-     "filter: invert(1) hue-rotate(180deg);"
-     "}\n");
-  CX("#pikchrshow-output-wrapper {"
-     "padding: 0.25em 0.5em; border-radius: 0.25em;"
-     "border-width: 1px;"/*some skins disable fieldset borders*/
-     "}\n");
-  CX("#pikchrshow-output-wrapper > legend > *:not(.copy-button){"
-     "margin-right: 0.5em; vertical-align: middle;"
-     "}\n");
-  CX("body.pikchrshow .v-align-middle{"
-     "vertical-align: middle"
-     "}\n");
-  CX(".dragover {border: 3px dotted rgba(0,255,0,0.6)}\n");
-  CX("</style>");
+  CX("<style>"); {
+    CX("div.content { padding-top: 0.5em }\n");
+    CX("#sbs-wrapper {"
+       "display: flex; flex-direction: column;"
+       "}\n");
+    CX("#sbs-wrapper > * {"
+       "margin: 0 0.25em 0.5em 0; flex: 1 10 auto;"
+       "align-self: stretch;"
+       "}\n");
+    CX("#sbs-wrapper textarea {"
+       "max-width: initial; flex: 1 1 auto;"
+       "}\n");
+    CX("#pikchrshow-output, #pikchrshow-form"
+       "{display: flex; flex-direction: column; align-items: stretch;}");
+    CX("#pikchrshow-form > * {margin: 0.25em 0}\n");
+    CX("#pikchrshow-output {flex: 5 1 auto; padding: 0}\n");
+    CX("#pikchrshow-output > pre, "
+       "#pikchrshow-output > pre > div, "
+       "#pikchrshow-output > pre > div > pre "
+       "{margin: 0; padding: 0}\n");
+    CX("#pikchrshow-output.error > pre "
+       /* Server-side error report */
+       "{padding: 0.5em}\n");
+    CX("#pikchrshow-controls {" /* where the buttons live */
+       "display: flex; flex-direction: row; "
+       "align-items: center; flex-wrap: wrap;"
+       "}\n");
+    CX("#pikchrshow-controls > * {"
+       "display: inline; margin: 0 0.25em 0.5em 0;"
+       "}\n");
+    CX("#pikchrshow-output-wrapper label {"
+       "cursor: pointer;"
+       "}\n");
+    CX("body.pikchrshow .input-with-label > * {"
+       "margin: 0 0.2em;"
+       "}\n");
+    CX("body.pikchrshow .input-with-label > label {"
+       "cursor: pointer;"
+       "}\n");
+    CX("#pikchrshow-output.dark-mode svg {"
+       /* Flip the colors to approximate a dark theme look */
+       "filter: invert(1) hue-rotate(180deg);"
+       "}\n");
+    CX("#pikchrshow-output-wrapper {"
+       "padding: 0.25em 0.5em; border-radius: 0.25em;"
+       "border-width: 1px;"/*some skins disable fieldset borders*/
+       "}\n");
+    CX("#pikchrshow-output-wrapper > legend > *:not(.copy-button){"
+       "margin-right: 0.5em; vertical-align: middle;"
+       "}\n");
+    CX("body.pikchrshow .v-align-middle{"
+       "vertical-align: middle"
+       "}\n");
+    CX(".dragover {border: 3px dotted rgba(0,255,0,0.6)}\n");
+  } CX("</style>");
   CX("<div>Input pikchr code and tap Preview to render it:</div>");
-  CX("<div id='sbs-wrapper'>");
-  CX("<div id='pikchrshow-form'>");
-  CX("<textarea id='content' name='content' rows='15'>%s</textarea>",
-     zContent/*safe-for-%s*/);
-  CX("<div id='pikchrshow-controls'>");
-  CX("<button id='pikchr-submit-preview'>Preview</button>");
-  style_labeled_checkbox("flipcolors-wrapper", "flipcolors",
-                         "Dark mode?",
-                         "1", isDark, 0);
-  CX("</div>"/*#pikchrshow-controls*/);
-  CX("</div>"/*#pikchrshow-form*/);
-  CX("<fieldset id='pikchrshow-output-wrapper'>");
-  CX("<legend></legend>"
-     /* Reminder: Firefox does not properly flexbox a LEGEND element,
-        always flowing it in column mode. */);
-  CX("<div id='pikchrshow-output'>");
-  if(*zContent){
-    int w = 0, h = 0;
-    char *zOut = pikchr(zContent, "pikchr", 0, &w, &h);
-    if( w>0 && h>0 ){
-      const char *zNonce = safe_html_nonce(1);
-      CX("%s<div style='max-width:%dpx;'>\n%s</div>%s",
-         zNonce, w, zOut, zNonce);
-    }else{
-      CX("<pre>\n%s\n</pre>\n", zOut);
+  CX("<div id='sbs-wrapper'>"); {
+    CX("<div id='pikchrshow-form'>"); {
+      CX("<textarea id='content' name='content' rows='15'>"
+         "%s</textarea>",zContent/*safe-for-%s*/);
+      CX("<div id='pikchrshow-controls'>"); {
+        CX("<button id='pikchr-submit-preview'>Preview</button>");
+        CX("<div class='input-with-label'>"); {
+          CX("<button id='pikchr-stash'>Stash</button>");
+          CX("<button id='pikchr-unstash'>Unstash</button>");
+          CX("<button id='pikchr-clear-stash'>Clear stash</button>");
+          CX("<span>Stores/restores a single pikchr script to/from "
+             "browser-local storage from/to the editor.</span>"
+             /* gets turned into a help-buttonlet */);
+        } CX("</div>"/*stash controls*/);
+        style_labeled_checkbox("flipcolors-wrapper", "flipcolors",
+                               "Dark mode?",
+                               "1", isDark, 0);
+      } CX("</div>"/*#pikchrshow-controls*/);
     }
-    fossil_free(zOut);
-  }
-  CX("</div>"/*#pikchrshow-output*/);
-  CX("</fieldset>"/*#pikchrshow-output-wrapper*/);
-  CX("</div>"/*sbs-wrapper*/);
+    CX("</div>"/*#pikchrshow-form*/);
+    CX("<fieldset id='pikchrshow-output-wrapper'>"); {
+      CX("<legend></legend>"
+         /* Reminder: Firefox does not properly flexbox a LEGEND
+            element, always flowing it in column mode. */);
+      CX("<div id='pikchrshow-output'>");
+      if(*zContent){
+        int w = 0, h = 0;
+        char *zOut = pikchr(zContent, "pikchr", 0, &w, &h);
+        if( w>0 && h>0 ){
+          const char *zNonce = safe_html_nonce(1);
+          CX("%s<div style='max-width:%dpx;'>\n%s</div>%s",
+             zNonce, w, zOut, zNonce);
+        }else{
+          CX("<pre>\n%s\n</pre>\n", zOut);
+        }
+        fossil_free(zOut);
+      } CX("</div>"/*#pikchrshow-output*/);
+    } CX("</fieldset>"/*#pikchrshow-output-wrapper*/);
+  } CX("</div>"/*sbs-wrapper*/);
   if(!builtin_bundle_all_fossil_js_apis()){
     builtin_emit_fossil_js_apis("dom", "fetch", "copybutton",
-                                "popupwidget", 0);
+                                "popupwidget", "storage", 0);
   }
   builtin_emit_fossil_js_apis("page.pikchrshow", 0);
   builtin_fulfill_js_requests();
