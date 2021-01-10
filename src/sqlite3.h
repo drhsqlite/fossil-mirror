a200f7a32f1db1c53b01c4aa011672b7a6d88eca
drh <drh@noemail.net>
2021-01-10 19:28:42 +0000

    Update to the built-in SQLite latest 3.35 alpha with performance enhancements and with the new ".stats vmstep" command for easier performance measurement.
    FossilOrigin-Name: 7bf929984a46ab608e5c639e2b2d23d95b9e4d767b50beacb4360d05119457fe

--- src/sqlite3.h
+++ src/sqlite3.h
@@ -125,7 +125,7 @@ extern "C" {
 */
 #define SQLITE_VERSION        "3.35.0"
 #define SQLITE_VERSION_NUMBER 3035000
-#define SQLITE_SOURCE_ID      "2021-01-01 22:06:17 d01e9f2d00dc439c529cd8885a219fcddbaad73b9f471b020e2a0c18e2add69b"
+#define SQLITE_SOURCE_ID      "2021-01-09 19:10:04 49dfce469e6a17111b349e53578479daf783064200bf0eec5bf8a91d3553b19f"
 
 /*
 ** CAPI3REF: Run-Time Library Version Numbers
