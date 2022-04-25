5da9748359af13e1f89aed3f61d51dee096096d9
florian <florian.balmer@gmail.com>
2022-04-25 05:21:00 +0000

    Follow-up to [346e45702c]: Remove the (unused) --sync option for the open command, as considering it and overriding local/global/pull-only/push-pull settings gets quite convoluted. Also mention that --force allows to continue if auto-sync fails.
    FossilOrigin-Name: b3ed9d37d5a5813edbc9e7b674c08eb0ceb572ebe35be603c2af52243419bba8

--- src/db.c
+++ src/db.c
@@ -3669,7 +3669,7 @@ void db_record_repository_filename(const char *zName){
 **                     with the local repository. If you commit this checkout,
 **                     it will become a new "initial" commit in the repository.
 **   -f|--force        Continue with the open even if the working directory is
-**                     not empty.
+**                     not empty, or if auto-sync fails.
 **   --force-missing   Force opening a repository with missing content
 **   -k|--keep         Only modify the manifest and manifest.uuid files
 **   --nested          Allow opening a repository inside an opened checkout
@@ -3680,8 +3680,6 @@ void db_record_repository_filename(const char *zName){
 **   --setmtime        Set timestamps of all files to match their SCM-side
 **                     times (the timestamp of the last checkin which modified
 **                     them).
-**   --sync            Auto-sync prior to opening even if the autosync setting
-**                     is off.
 **   --verbose         If passed a URI then this flag is passed on to the clone
 **                     operation, otherwise it has no effect.
 **   --workdir DIR     Use DIR as the working directory instead of ".". The DIR
