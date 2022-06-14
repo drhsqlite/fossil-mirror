c7c6d9427d445506d0e20f457835610afd9c3402
stephan <sgbeal@googlemail.com>
2022-06-14 17:42:17 +0000

    Latest upstream pikchr.c to enable HTML entities which contain digits.
    FossilOrigin-Name: b1ffe209f3c40ed1b6d2a9c398e2780da8bdf0716bde304d874eec52968f67a4

--- extsrc/pikchr.c
+++ extsrc/pikchr.c
@@ -4540,7 +4540,8 @@ static void pik_append(Pik *p, const char *zText, int n){
 ** with a construct which syntactically matches an HTML entity escape
 ** sequence (without checking for whether it's a known entity). Always
 ** returns false if zText[0] is false or n<4. Entities match the
-** equivalent of the regexes `&#[0-9]+;` and `&[a-zA-Z]+;`.
+** equivalent of the regexes `&#[0-9]{2,};` and
+** `&[a-zA-Z][a-zA-Z0-9]+;`.
 */
 static int pik_isentity(char const * zText, int n){
   int i = 0;
@@ -4553,11 +4554,14 @@ static int pik_isentity(char const * zText, int n){
     for(i=0; i<n; i++){
       if( i>1 && ';'==zText[i] ) return 1;
       else if( zText[i]<'0' || zText[i]>'9' ) return 0;
+      /* Note that &#nn; values nn<32d are not legal entities. */
     }
   }else{
-    for( i=0; i<n; i++ ){
+    for(i=0; i<n; i++){
       if( i>1 && ';'==zText[i] ) return 1;
-      else if( zText[i]<'A' || zText[i]>'z'
+      else if( i>0 && zText[i]>='0' && zText[i]<='9' ){
+          continue;
+      }else if( zText[i]<'A' || zText[i]>'z'
                || (zText[i]>'Z' && zText[i]<'a') ) return 0;
     }
   }
@@ -8129,4 +8133,4 @@ int Pikchr_Init(Tcl_Interp *interp){
 #endif /* PIKCHR_TCL */
 
 
-#line 8157 "pikchr.c"
+#line 8161 "pikchr.c"
