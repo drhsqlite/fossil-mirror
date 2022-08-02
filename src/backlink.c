7ef4ec848a80d123f8b6190d078fee5ec0dd5e12
drh <drh@sqlite.org>
2022-08-02 20:55:10 +0000

    Put all sections of the /vinfo page (or the /info page for a check-in) inside an accordion widget.
    FossilOrigin-Name: ab553393a1675e36200fe2c34c15c5c57255967235abae4058d6c75a27d68b43

--- src/backlink.c
+++ src/backlink.c
@@ -36,6 +36,7 @@ void render_backlink_graph(const char *zUuid, const char *zLabel){
   Blob sql;
   Stmt q;
   char *zGlob;
+  int needEndPanel = 0;
   zGlob = mprintf("%.5s*", zUuid);
   db_multi_exec(
      "CREATE TEMP TABLE IF NOT EXISTS ok(rid INTEGER PRIMARY KEY);\n"
@@ -51,7 +52,13 @@ void render_backlink_graph(const char *zUuid, const char *zLabel){
      zGlob, zUuid
   );
   if( !db_exists("SELECT 1 FROM ok") ) return;
-  if( zLabel ) cgi_printf("%s", zLabel);
+  if( zLabel ){
+    cgi_printf("%s", zLabel);
+    if( strstr(zLabel, "accordion")!=0 ){
+      cgi_printf("<div class=\"accordion_panel\">\n");
+      needEndPanel = 1;
+    }
+  }
   blob_zero(&sql);
   blob_append(&sql, timeline_query_for_www(), -1);
   blob_append_sql(&sql, " AND event.objid IN ok ORDER BY mtime DESC");
@@ -60,6 +67,9 @@ void render_backlink_graph(const char *zUuid, const char *zLabel){
       TIMELINE_DISJOINT|TIMELINE_GRAPH|TIMELINE_NOSCROLL|TIMELINE_REFS,
                      0, 0, 0, 0, 0, 0);
   db_finalize(&q);
+  if( needEndPanel ){
+    cgi_printf("</div>\n");
+  }
 }
 
 /*
