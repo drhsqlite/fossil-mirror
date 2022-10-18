765bb63d3a54bf1d91ba15c0b046c3fa8f785b8d
florian <florian.balmer@gmail.com>
2022-10-18 13:30:59 +0000

    Cherry-pick [767b175d90]: Avoid references to private check-ins in Q-cards of public check-in manifests. This ensures consistent behavior regarding leakage of private check-in hashes, possibly generating phantoms on peer repositories, for P- and Q-cards.
    FossilOrigin-Name: 52a66829d655ff35dc5245b664b86225438d7f46a8d183ac98268ece982d8564

--- src/checkin.c
+++ src/checkin.c
@@ -1771,9 +1771,8 @@ static void create_manifest(
   while( db_step(&q)==SQLITE_ROW ){
     const char *zCherrypickUuid = db_column_text(&q, 0);
     int mid = db_column_int(&q, 1);
-    if( mid != vid ){
-      blob_appendf(pOut, "Q %s\n", zCherrypickUuid);
-    }
+    if( (!g.markPrivate && content_is_private(mid)) || (mid == vid) ) continue;
+    blob_appendf(pOut, "Q %s\n", zCherrypickUuid);
   }
   db_finalize(&q);
 
