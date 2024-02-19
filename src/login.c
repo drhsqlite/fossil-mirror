f0562916dab00713bcdfbfc402354d7daf382294
stephan <sgbeal@googlemail.com>
2024-02-19 16:18:52 +0000

    Add a link to /skins on the /login page, per request in the forum.
    FossilOrigin-Name: cc7930e13cc8caad035efb4f40a0604887ac819d3a5c4c08936ae61b447de606

--- src/login.c
+++ src/login.c
@@ -855,6 +855,9 @@ void login_page(void){
       @ <a href="%R/timeline?ss=v&y=f&vfx&u=%t(g.zLogin)">Forum
       @ post timeline</a> for user <b>%h(g.zLogin)</b></p>
     }
+    @ <hr><p>
+    @ Select your preferred <a href="%R/skins">site skin</a>.
+    @ </p>
     if( g.perm.Password ){
       char *zRPW = fossil_random_password(12);
       @ <hr>
