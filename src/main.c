660278e2382d023b8be470a3a41ff7a7b22e47ef
drh <drh@sqlite.org>
2024-03-29 19:23:21 +0000

    Do not issue error-log panics for timeouts unless the total CPU time exceeds 10 seconds.
    FossilOrigin-Name: d6fdfd372b8da9b841554b3f7d32fe3973d1bb74f2af48172379586f1151b650

--- src/main.c
+++ src/main.c
@@ -3021,10 +3021,10 @@ static void sigalrm_handler(int x){
   sqlite3_uint64 tmUser = 0, tmKernel = 0;
   fossil_cpu_times(&tmUser, &tmKernel);
   if( fossil_strcmp(g.zPhase, "web-page reply")==0
-   && tmUser+tmKernel<1000000
+   && tmUser+tmKernel<10000000
   ){
     /* Do not log time-outs during web-page reply unless more than
-    ** 1 second of CPU time has been consumed */
+    ** 10 seconds of CPU time has been consumed */
     return;
   }
   fossil_panic("Timeout after %d seconds during %s"
