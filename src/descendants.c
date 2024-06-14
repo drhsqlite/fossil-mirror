8fc8c7e7079c8a49264bcdba9521cce453ad3d23
drh <drh@sqlite.org>
2024-06-14 00:06:10 +0000

    Temporary fix for a bad query plan caused by issues in SQLite.  See [forum:/forumpost/6900455c28dcd3da|forum post 6900455c28dcd3da].
    FossilOrigin-Name: 9866c97b357062f8ef3c3f5a5aa92c065d0de1f3d82655d0a4cb343dd44aeccf

--- src/descendants.c
+++ src/descendants.c
@@ -249,7 +249,7 @@ int compute_youngest_ancestor_in_branch(int rid, const char *zBranch){
     "  )"
     "  SELECT ancestor.rid FROM ancestor"
     "   WHERE EXISTS(SELECT 1 FROM tagxref"
-                    " WHERE tagid=%d AND tagxref.rid=ancestor.rid"
+                    " WHERE tagid=%d AND tagxref.rid=+ancestor.rid"
                     "   AND value=%Q AND tagtype>0)"
     "  LIMIT 1",
     rid, rid, TAG_BRANCH, zBranch
