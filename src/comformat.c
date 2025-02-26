4d11ba122c053514174767769c6110f798b081c6
drh <drh@sqlite.org>
2025-02-26 21:03:37 +0000

    Fix the comment formatter so that it does not crash with ill-formatted VT100 escapes.
    FossilOrigin-Name: 2aa79ed7521b6c6c732ccd2a95d8b320557e5732cd219fad437de5ee56fdfa3d

--- src/comformat.c
+++ src/comformat.c
@@ -265,10 +265,12 @@ void char_info_utf8(
   if( c==0x1b && z[i]=='[' ){
     do{
       i++;
-    }while( fossil_isdigit(z[i]) || z[i]==';' );
-    *pCchUTF8 = i+1;
-    *pUtf32 = 0x301;  /* A zero-width character */
-    return;
+    }while( i<fossil_isdigit(z[i]) || z[i]==';' );
+    if( fossil_isalpha(z[i]) ){
+      *pCchUTF8 = i+1;
+      *pUtf32 = 0x301;  /* A zero-width character */
+      return;
+    }
   }
   if( (c&0x80)==0x00 ){                   /* 7-bit ASCII character. */
     *pCchUTF8 = 1;
