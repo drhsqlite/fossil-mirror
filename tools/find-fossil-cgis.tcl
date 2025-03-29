a6ca119503ee5ecdf8cab1a1798c31049fa3cef6
drh <drh@sqlite.org>
2025-03-29 10:44:12 +0000

    New command-line options for find-fossil-cgis.tcl:  --print, --symlink, and -v.
    FossilOrigin-Name: 609f885a02afc17c3573314255eccb9a94c37e94af7553ea6ae1b1bc711f0922

--- tools/find-fossil-cgis.tcl
+++ tools/find-fossil-cgis.tcl
@@ -2,10 +2,10 @@
 #
 # This script scans a directory hierarchy looking for Fossil CGI files -
 # the files that are used to launch Fossil as a CGI program.  For each
-# such file found, in prints the name of the file and then prints the
-# file content, indented.
+# such file found, in prints the name of the file and also the file
+# content, indented, if the --print option is used.
 #
-#     tclsh find-fossil-cgis.tcl [OPTIONS] $DIRECTORY
+#     tclsh find-fossil-cgis.tcl [OPTIONS] DIRECTORY
 #
 # The argument is the directory from which to begin the search.
 #
@@ -18,13 +18,23 @@
 #    --hasnot REGEXP            Only show the CGI if it does NOT match the
 #                               REGEXP.
 #
-
+#    --print                    Show the content of the CGI, indented by
+#                               three spaces
+#
+#    --symlink                  Process DIRECTORY arguments that are symlinks.
+#                               Normally symlinks are silently ignored.
+#
+#    -v                         Show progress information for debugging
+#
 
 # Find the CGIs in directory $dir.  Invoke recursively to
 # scan subdirectories.
 #
 proc find_in_one_dir {dir} {
-  global HAS HASNOT
+  global HAS HASNOT PRINT V
+  if {$V>0} {
+    puts "# $dir"
+  }
   foreach obj [lsort [glob -nocomplain -directory $dir *]] {
     if {[file isdir $obj]} {
       find_in_one_dir $obj
@@ -32,6 +42,7 @@ proc find_in_one_dir {dir} {
     }
     if {![file isfile $obj]} continue
     if {[file size $obj]>5000} continue
+    if {![file exec $obj]} continue
     if {![file readable $obj]} continue
     set fd [open $obj rb]
     set txt [read $fd]
@@ -54,13 +65,23 @@ proc find_in_one_dir {dir} {
     # At this point assume we have found a CGI file.
     #
     puts $obj
-    regsub -all {\n} [string trim $txt] "\n   " out
-    puts "   $out"
+    if {$PRINT} {
+      regsub -all {\n} [string trim $txt] "\n   " out
+      puts "   $out"
+    }
   }
 }
 set HAS [list]
 set HASNOT [list]
+set PRINT 0
+set SYMLINK 0
+set V 0
 set N [llength $argv]
+set DIRLIST [list]
+
+# First pass:  Gather all the command-line arguments but do no
+# processing.
+#
 for {set i 0} {$i<$N} {incr i} {
   set dir [lindex $argv $i]
   if {($dir eq "-has" || $dir eq "--has") && $i<[expr {$N-1}]} {
@@ -73,5 +94,28 @@ for {set i 0} {$i<$N} {incr i} {
     lappend HASNOT [lindex $argv $i]
     continue
   }
-  find_in_one_dir $dir
+  if {$dir eq "-print" || $dir eq "--print"} {
+    set PRINT 1
+    continue
+  }
+  if {$dir eq "-symlink" || $dir eq "--symlink"} {
+    set SYMLINK 1
+    continue
+  }
+  if {$dir eq "-v"} {
+    set V 1
+    continue
+  }
+  if {[file type $dir]=="directory"} {
+    lappend DIRLIST $dir
+  }
+}
+
+# Second pass: Process the non-option arguments.
+#
+foreach dir $DIRLIST {
+  set type [file type $dir]
+  if {$type eq "directory" || ($SYMLINK && $type eq "link")} {
+    find_in_one_dir $dir
+  }
 }
