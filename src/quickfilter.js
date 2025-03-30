07e0621dd4c6ee13c7bbf45231c4c63467903660
jkosche <jkosche@noemail.net>
2025-03-30 12:20:23 +0000

    no more need to display the quickfilter based on elements, as it is now explicitly activated
    FossilOrigin-Name: 32d86c59ab1d73ed7bcd87a40b0f9587cdd58daffc40de018a310b5672cb513a

--- src/quickfilter.js
+++ src/quickfilter.js
@@ -1,27 +1,17 @@
 /* Javascript code that will enable quick filtering of items in tables.
-** 
+**
 ** Add an input field with the id 'quickfilter' as follows:
-**   <input type="text" id="quickfilter" placeholder="filter list..."
-**    style="display: none">
+**   <input type="text" id="quickfilter" placeholder="filter list...">
 ** Mark the table with the filter items with the class 'filterlist'.
 ** The table is expected to have a tbody around the rows that are
 ** filtered (to avoid filtering the header).
 **
-** The input field is hidden at standard ('display:none'), but the script
-** will display it, if the list contains more than five elements.
-**
-** If shown the user can type to filter the table for elements matching
-** the typed text.
+** The user can type to filter the table for elements matching the typed text.
 */
 
-const quickfilter = document.getElementById('quickfilter');
-const filterlist = document.querySelectorAll('.filterlist tbody tr');
-
-document.addEventListener('DOMContentLoaded', function(){
-  if (filterlist.length > 5) quickfilter.style.display = '';
-});
-
 quickfilter.addEventListener('input', function (){
+  const quickfilter = document.getElementById('quickfilter');
+  const filterlist = document.querySelectorAll('.filterlist tbody tr');
   const filter = quickfilter.value.toLowerCase().trim();
   filterlist.forEach(function(row){
     const rowText = row.textContent.toLowerCase().trim();
