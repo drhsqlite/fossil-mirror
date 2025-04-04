66442bd53338003ba4ebb40fd6e3903ab5f22824
jkosche <jkosche@noemail.net>
2025-04-04 19:32:25 +0000

    quickfilter also highlights the found matches
    FossilOrigin-Name: cd31dea4729a5f8134ca5586c5b279bab3f6705631d6ec69451046c016b1baea

--- src/quickfilter.js
+++ src/quickfilter.js
@@ -9,12 +9,34 @@
 ** The user can type to filter the table for elements matching the typed text.
 */
 
-quickfilter.addEventListener('input', function (){
+quickfilter.addEventListener('input', function(){
   const quickfilter = document.getElementById('quickfilter');
   const filterlist = document.querySelectorAll('.filterlist tbody tr');
   const filter = quickfilter.value.toLowerCase().trim();
-  filterlist.forEach(function(row){
-    const rowText = row.textContent.toLowerCase().trim();
-    row.style.display = rowText.includes(filter) ? 'table-row' : 'none';
-  });
+  for(row of filterlist){
+    const orig = row.innerHTML;
+    const cleaned = orig.replaceAll("<mark>", "").replaceAll("</mark>", "");
+    if(filter===''){
+      row.innerHTML = cleaned;
+      row.style.display = 'table-row';
+      continue;
+    }
+    let ind = cleaned.toLowerCase().lastIndexOf(filter);
+    if(ind<0){
+      row.innerHTML = cleaned;
+      row.style.display = 'none';
+    }
+    let marked = cleaned;
+    do{
+      if(cleaned.lastIndexOf('<',ind-1)<cleaned.lastIndexOf('>',ind-1)){
+        // not inside a tag
+        marked = marked.substring(0,ind)+'<mark>'+
+          marked.substring(ind, ind+filter.length)+'</mark>'+
+          marked.substring(ind+filter.length);
+      }
+      ind = cleaned.toLowerCase().lastIndexOf(filter,ind-1);
+    }while(ind>=0);
+    row.style.display = (marked===cleaned) ? 'none' : 'table-row';
+    row.innerHTML = marked;
+  };
 });
