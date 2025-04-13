bb2577a3622a6bbe5ed30ce7dca079ac8507b1ee
jkosche <jkosche@noemail.net>
2025-04-13 12:39:41 +0000

    better quoting to make intent clearer, thanks to [forum:/forumpost/cf724a9bea]
    FossilOrigin-Name: 3e16be4e027be739428ceff4ef582b1811f859171656979e983e3a1245c37226

--- src/tktsetup.c
+++ src/tktsetup.c
@@ -523,14 +523,14 @@ static const char zDefaultView[] =
 @ <th1>
 @ set versionlink ""
 @ set urlfoundin [httpize $foundin]
-@ set tagpattern ^\[-0-9A-Za-z_\\.\]+\$
+@ set tagpattern {^[-0-9A-Za-z_\\.]+$}
 @ if [regexp $tagpattern $foundin] {
 @   query {SELECT count(*) AS match FROM tag
 @          WHERE tagname=concat('sym-',$foundin)} {
 @     if {$match} {set versionlink "/timeline?t=$urlfoundin"}
 @   }
 @ }
-@ set hashpattern ^\[0-9a-f\]+\$
+@ set hashpattern {^[0-9a-f]+$}
 @ if [regexp $hashpattern $foundin] {
 @   set pattern $foundin*
 @   query {SELECT count(*) AS match FROM blob WHERE uuid GLOB $pattern} {
