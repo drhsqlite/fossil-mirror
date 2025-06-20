d9b86e32d77b78d91cc0e4de6094d1ac6a6a378f
stephan <sgbeal@googlemail.com>
2025-06-20 13:57:32 +0000

    Add link-tester.json, a set of links for link-tester.html (format is described near the bottom of link-tester.js). Add a link to link-tester which opens the current page in a new tab.
    FossilOrigin-Name: 1acf766268326759e6bffc1ba50076d630939d5c25b513e2a3839acd99cbf1ed

--- test/link-tester.js
+++ test/link-tester.js
@@ -16,12 +16,7 @@ window.addEventListener("DOMContentLoaded", function(){
   };
   const eIframe = E('#iframe');
   const eSelect = E('#selectPage');
-  eSelect.addEventListener('change',function(ev){
-    const so = ev.target.options[ev.target.selectedIndex];
-    if( so ){
-      eIframe.setAttribute('src', so.value || so.innerText);
-    }
-  });
+  const eCurrentUrl = E('#currentUrl');
 
   /*
     Prepend the fossil instance's URL to each link. We have to guess
@@ -32,7 +27,8 @@ window.addEventListener("DOMContentLoaded", function(){
   let urlTop;
   let loc = (''+window.location);
   let aLoc = loc.split('/')
-  aLoc.pop(); /* this file name */
+  aLoc.pop(); /* file name */
+  const thisDir = aLoc.join('/');
   const rxDoc = /.*\/doc\/[^/]+\/.*/;
   //console.log(rxDoc, loc, aLoc);
   if( loc.match(rxDoc) ){
@@ -52,16 +48,45 @@ window.addEventListener("DOMContentLoaded", function(){
   const eBtnPrev = E('#btn-prev');
   const eBtnNext = E('#btn-next');
 
+  const updateUrl = function(opt){
+    if( opt ){
+      let url = (opt.value || opt.innerText);
+      eCurrentUrl.innerText = url.replace(urlTop,'');
+      eCurrentUrl.setAttribute('href', url);
+    }else{
+      eCurrentUrl.innerText = '';
+    }
+  };
+
+  eSelect.addEventListener('change',function(ev){
+    const so = ev.target.options[ev.target.selectedIndex];
+    if( so ){
+      eIframe.setAttribute('src', so.value || so.innerText);
+      updateUrl(so);
+    }
+  });
+
+  const selectEntry = function(ndx){
+    if( ndx>=0 ){
+      eSelect.selectedIndex = ndx;
+      eSelect.dispatchEvent(new Event('change',{target:eSelect}));
+    }
+  };
+
   const cycleLink = function(dir){
     let n = eSelect.selectedIndex + dir;
     if( n < 0 ) n = eSelect.options.length-1;
     else if( n>=eSelect.options.length ){
       n = 0;
     }
-    if( n>=0 ){
+    const opt = eSelect.options[n];
+    if( opt && opt.disabled ){
+      /* If that OPTION element is disabled, skip over it. */
       eSelect.selectedIndex = n;
+      cycleLink(dir);
+    }else{
+      selectEntry(n);
     }
-    eSelect.dispatchEvent(new Event('change',{target:eSelect}));
   };
 
   eBtnPrev.addEventListener('click', ()=>cycleLink(-1), false);
@@ -129,7 +154,7 @@ window.addEventListener("DOMContentLoaded", function(){
       eToAvoid.forEach((e)=>e ? extra += effectiveHeight(e) : false);
       ht = wh - extra;
       if( ht < 100 ) ht = 100;
-      eConstrained.style.top = 'calc('+extra+'px + 0.25em)';
+      eConstrained.style.top = 'calc('+extra+'px + 1.5em)';
       eConstrained.style.height =
         eConstrained.style.maxHeight = [
           "calc(", ht, "px",
@@ -150,5 +175,50 @@ window.addEventListener("DOMContentLoaded", function(){
   delete ForceResizeKludge.$disabled;
   ForceResizeKludge();
 
-  eSelect.dispatchEvent(new Event('change',{target:eSelect}));
+  selectEntry(0);
+
+  /**
+     Read link-tester.json, which should live in the same directory
+     as this file. It's expected to be an array with entries
+     in one of the following forms:
+
+     - "string"   = Separator label (disabled)
+     - ["/path"]  = path with itself as a label
+     - ["label", "/path"] = path with the given label
+
+     All paths are expected to have a "/" prefix and this script
+     accounts for mapping that to the fossil part of this script's
+     URL.
+  */
+  window.fetch(thisDir+'/link-tester.json').then((r)=>r.json()).then(j=>{
+    //console.log("fetched",j);
+    eSelect.innerHTML = '';
+    const opt = function(arg){
+      const o = document.createElement('option');
+      //console.warn(arguments);
+      let rc = true;
+      if( 'string' === typeof arg ){
+        /* Grouping separator */
+        o.innerText = "--- " + arg + " ---";
+        o.setAttribute('disabled','');
+        rc = false;
+      }else if( 1===arg.length ){
+        o.innerText = arg[0];
+        o.value = urlTop + arg[0];
+      }else if( 2==arg.length ){
+        o.innerText = arg[0];
+        o.value = urlTop + arg[1];
+      }
+      eSelect.appendChild(o);
+      return rc;
+    };
+    let ndx = -1/*index of first non-disabled entry*/, i = 0;
+    for(const e of j){
+      if( opt(e) && ndx<0 ){
+        ndx = i;
+      }
+      ++i;
+    }
+    selectEntry(ndx);
+  });
 });
