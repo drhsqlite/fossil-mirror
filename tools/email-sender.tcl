85525ac0049def695d5d97d4298a166dd714d62a
drh <drh@sqlite.org>
2025-10-23 13:16:00 +0000

    Update the email notification transfer daemon script.
    FossilOrigin-Name: 4163ef006d7067a686f24b9fc0aa6572e9df1f3a777d08486814b3af205621be

--- tools/email-sender.tcl
+++ tools/email-sender.tcl
@@ -18,27 +18,48 @@ db eval {
     emailid INTEGER PRIMARY KEY,
     msg TXT
   );
+  CREATE TABLE IF NOT EXISTS sentlog(
+    mtime INT,
+    xto TEXT,
+    xfrom TEXT,
+    xsubject TEXT,
+    xsize INT
+  );
 }
+set ctr 0
 while {1} {
   set n 0
   db transaction immediate {
     set emailid 0
     db eval {SELECT emailid, msg FROM email LIMIT 1} {
       set pipe $PIPE
-      if {[regexp {\nFrom:[^\n]*<([^>]+)>} $msg all addr]} {
-        append pipe " -f $addr"
+      set to unk
+      set subject none
+      set size [string length $msg]
+      regexp {To:[^\n]*<([^>]+)>} $msg all to
+      regexp {\nSubject:[ ]*([^\r\n]+)} $msg all subject
+      set subject [string trim $subject]
+      if {[regexp {\nFrom:[^\n]*<([^>]+)>} $msg all from]} {
+        append pipe " -f $from"
       }
       set out [open |$pipe w]
       puts -nonewline $out $msg
       flush $out
       close $out
       incr n
+      incr ctr
     }
     if {$n>0} {
       db eval {DELETE FROM email WHERE emailid=$emailid}
+      db eval {INSERT INTO sentlog(mtime,xto,xfrom,xsubject,xsize)
+               VALUES(unixepoch(),$to,$from,$subject,$size)}
     }
   }
   if {$n==0} {
+    if {$ctr>100} {
+      db eval {DELETE FROM sentlog WHERE mtime<unixepoch('now','-30 days')}
+      set ctr 0
+    }
     after $POLLING_INTERVAL
   }
 }
