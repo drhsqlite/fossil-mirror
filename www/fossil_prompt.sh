05746daca1c3814e1de9d96e8e9e05406ecc0784
drh <drh@sqlite.org>
2025-11-20 00:40:42 +0000

    Update the www/fossil_prompt.sh demonstration script to the latest version used by the lead SQLite developer.
    FossilOrigin-Name: 077430469f6bc2ca4dadd6440f90bfb1cb712e7b1eb6ebfff5d890bf91f072f8

--- www/fossil_prompt.sh
+++ www/fossil_prompt.sh
@@ -1,56 +1,14 @@
-#-------------------------------------------------------------------------
-#   get_fossil_data()
-#
-# If the current directory is part of a fossil checkout, then populate
-# a series of global variables based on the current state of that
-# checkout. Variables are populated based on the output of the [fossil info]
-# command.
-#
-# If the current directory is not part of a fossil checkout, set global
-# variable $fossil_info_project_name to an empty string and return.
-#
-function get_fossil_data() {
-  fossil_info_project_name=""
-  eval `get_fossil_data2`
-}
-function get_fossil_data2() {
-  fossil info 2> /dev/null |tr '\042\047\140' _|grep "^[^ ]*:" |
-  while read LINE ; do
-    local field=`echo $LINE | sed 's/:.*$//' | sed 's/-/_/'`
-    local value=`echo $LINE | sed 's/^[^ ]*: *//'`
-    echo fossil_info_${field}=\"${value}\"
-  done
-}
-
-#-------------------------------------------------------------------------
-#   set_prompt()
-#
-# Set the PS1 variable. If the current directory is part of a fossil
-# checkout then the prompt contains information relating to the state
-# of the checkout. 
-#
-# Otherwise, if the current directory is not part of a fossil checkout, it
-# is set to a fairly standard bash prompt containing the host name, user
-# name and current directory.
-#
 function set_prompt() {
-  get_fossil_data
-  if [ -n "$fossil_info_project_name" ] ; then 
-    # Color the path part of the prompt blue if this is a clean checkout
-    # Or red if it has been edited in any way at all. Set $c1 to the escape
-    # sequence required to change the type to the required color. And $c2
-    # to the sequence that changes it back.
-    #
-    if [ -n "`fossil chang`" ] ; then
-      c1="\[\033[1;31m\]"           # red
-    else
-      c1="\[\033[1;34m\]"           # blue
-    fi
-    c2="\[\033[0m\]"
-    PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:$c1\w\$$c2 "
-  else
-    PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\w\$ "
-  fi
+  case `fossil status -b` in
+    clean)
+       PS1="\[\e[1;32m\]\u@\h\[\e[0m\]:\[\e[1;36m\]\w\$\[\e[0m\] "
+       ;;
+    dirty)
+       PS1="\[\e[1;32m\]\u@\h\[\e[0m\]:\[\e[38;5;202m\]\w\$\[\e[0m\] "
+       ;;
+    *)
+       PS1="\[\e[1;32m\]\u@\h\[\e[0m\]:\w\$ "
+       ;;
+  esac
 }
-
 PROMPT_COMMAND=set_prompt
