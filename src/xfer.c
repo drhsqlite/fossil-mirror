cb86ff6b578de38690d5887ebeacf0280fb39238
drh <drh@sqlite.org>
2026-08-05 18:09:08 +0000

    The "clone" privilege (letter "g") implies the ability to "fossil pull".
    FossilOrigin-Name: e81cac374b73f1c0ebe4cb178cf2a4700e5057a6b05155c4405f4a9062e17f21

--- src/xfer.c
+++ src/xfer.c
@@ -1298,6 +1298,7 @@ void page_xfer(void){
   g.zLogin = "anonymous";
   login_set_anon_nobody_capabilities();
   login_check_credentials();
+  if( g.perm.Clone ) g.perm.Read = 1;
   cgi_check_for_malice();
   memset(&xfer, 0, sizeof(xfer));
   blobarray_zero(xfer.aToken, count(xfer.aToken));
